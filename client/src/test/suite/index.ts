b1f8a20d7ff4b174e93d75f8699a3abf0bf30c0b
Vinicius Stock <vinicius.stock@shopify.com>
2022-03-03 13:38:30 -0500

    Add eslint and prettier config

--- client/src/test/suite/index.ts
+++ client/src/test/suite/index.ts
@@ -1,38 +1,40 @@
-import * as path from 'path';
-import * as Mocha from 'mocha';
-import * as glob from 'glob';
+import * as path from "path";
+
+import * as Mocha from "mocha";
+import * as glob from "glob";
 
 export function run(): Promise<void> {
-	// Create the mocha test
-	const mocha = new Mocha({
-		ui: 'tdd',
-		color: true
-	});
+  // Create the mocha test
+  const mocha = new Mocha({
+    ui: "tdd",
+    color: true,
+  });
 
-	const testsRoot = path.resolve(__dirname, '..');
+  const testsRoot = path.resolve(__dirname, "..");
 
-	return new Promise((c, e) => {
-		glob('**/**.test.js', { cwd: testsRoot }, (err, files) => {
-			if (err) {
-				return e(err);
-			}
+  return new Promise((resolve, reject) => {
+    glob("**/**.test.js", { cwd: testsRoot }, (globError, files) => {
+      if (globError) {
+        return reject(globError);
+      }
 
-			// Add files to the test suite
-			files.forEach(f => mocha.addFile(path.resolve(testsRoot, f)));
+      // Add files to the test suite
+      files.forEach((file) => mocha.addFile(path.resolve(testsRoot, file)));
 
-			try {
-				// Run the mocha test
-				mocha.run(failures => {
-					if (failures > 0) {
-						e(new Error(`${failures} tests failed.`));
-					} else {
-						c();
-					}
-				});
-			} catch (err) {
-				console.error(err);
-				e(err);
-			}
-		});
-	});
+      try {
+        // Run the mocha test
+        mocha.run((failures) => {
+          if (failures > 0) {
+            reject(new Error(`${failures} tests failed.`));
+          } else {
+            resolve();
+          }
+        });
+      } catch (err) {
+        // eslint-disable-next-line no-console
+        console.error(err);
+        reject(err);
+      }
+    });
+  });
 }
