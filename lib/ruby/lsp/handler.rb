c8ce1c9d2494c20aea51784840cbe63bd33df7c3
Vinicius Stock <vinistock@users.noreply.github.com>
2022-03-23 13:34:42 -0400

    Remove capabilities we don't actually support (#12)

--- lib/ruby/lsp/handler.rb
+++ lib/ruby/lsp/handler.rb
@@ -49,11 +49,6 @@ module Ruby
             text_document_sync: Interface::TextDocumentSyncOptions.new(
               change: Constant::TextDocumentSyncKind::FULL
             ),
-            completion_provider: Interface::CompletionOptions.new(
-              resolve_provider: true,
-              trigger_characters: ["."]
-            ),
-            definition_provider: true
           )
         )
       end
