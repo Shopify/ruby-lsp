23beecaaadec60b898c6191edb7e59fc0929ce99
Vinicius Stock <vinicius.stock@shopify.com>
2022-03-23 13:07:57 -0400

    Use the language server gem folding ranges class

--- lib/ruby/lsp/requests/folding_ranges.rb
+++ lib/ruby/lsp/requests/folding_ranges.rb
@@ -20,33 +20,19 @@ module Ruby
 
             case node
             when SyntaxTree::Def
-              @ranges << CodeRange.new(node.location, "region")
+              location = node.location
+
+              @ranges << LanguageServer::Protocol::Interface::FoldingRange.new(
+                start_line: location.start_line - 1,
+                end_line: location.end_line - 1,
+                kind: "region"
+              )
             else
               @queue.unshift(*node.child_nodes.compact)
             end
           end
 
           @ranges
-        end
-
-        class CodeRange
-          KINDS = ["comment", "imports", "region"].freeze
-
-          def initialize(location, kind)
-            raise ArgumentError, "Invalid folding range kind: #{kind}" unless KINDS.include?(kind)
-
-            @start_line = location.start_line - 1
-            @end_line = location.end_line - 1
-            @kind = kind
-          end
-
-          def to_json(*)
-            {
-              startLine: @start_line,
-              endLine: @end_line,
-              kind: @kind,
-            }.to_json
-          end
         end
       end
     end
