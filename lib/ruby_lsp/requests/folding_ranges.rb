dc470a36de699f98346d193a74aec97186becba4
Vinicius Stock <vinistock@users.noreply.github.com>
2022-03-30 09:57:58 -0400

    Add folding support for begin,rescue,ensure and multiline invocations with no parenthesis (#24)
    * Add support for begin,rescue,ensure and multiline invocations with no parenthesis
    
    * Extract common statement functionality into a separate method
    
    * Combine add_statement_range into visit_statement_node
    
    * Make all visit methods private
    
    * Skip visiting statements if they're empty

--- lib/ruby_lsp/requests/folding_ranges.rb
+++ lib/ruby_lsp/requests/folding_ranges.rb
@@ -9,7 +9,6 @@ module RubyLsp
         SyntaxTree::ModuleDeclaration,
         SyntaxTree::DoBlock,
         SyntaxTree::BraceBlock,
-        SyntaxTree::ArrayLiteral,
         SyntaxTree::HashLiteral,
         SyntaxTree::If,
         SyntaxTree::Unless,
@@ -17,7 +16,7 @@ module RubyLsp
         SyntaxTree::While,
         SyntaxTree::Until,
         SyntaxTree::For,
-        SyntaxTree::ArgParen,
+        SyntaxTree::Args,
         SyntaxTree::Heredoc,
       ].freeze
 
@@ -37,6 +36,8 @@ module RubyLsp
         @ranges
       end
 
+      private
+
       # For nodes that are simple to fold, we just re-use the same method body
       SIMPLE_FOLDABLES.each do |node_class|
         class_eval(<<~RUBY, __FILE__, __LINE__ + 1)
@@ -45,6 +46,24 @@ module RubyLsp
             super
           end
         RUBY
+      end
+
+      def visit_arg_paren(node)
+        add_simple_range(node)
+      end
+
+      def visit_array_literal(node)
+        add_simple_range(node)
+
+        visit_all(node.contents.parts) if node.contents
+      end
+
+      def visit_begin(node)
+        unless node.bodystmt.statements.empty?
+          add_range(node.location.start_line - 1, node.bodystmt.statements.location.end_line - 1)
+        end
+
+        super
       end
 
       def visit_def(node)
@@ -60,26 +79,23 @@ module RubyLsp
       end
       alias_method :visit_defs, :visit_def
 
-      def visit_else(node)
-        add_statements_range(node)
-        super
-      end
-      alias_method :visit_elsif, :visit_else
-      alias_method :visit_when, :visit_else
+      def visit_statement_node(node)
+        return if node.statements.empty?
 
-      private
+        add_range(node.location.start_line - 1, node.statements.location.end_line - 1)
+        visit_all(node.child_nodes)
+      end
+      alias_method :visit_else, :visit_statement_node
+      alias_method :visit_elsif, :visit_statement_node
+      alias_method :visit_when, :visit_statement_node
+      alias_method :visit_ensure, :visit_statement_node
+      alias_method :visit_rescue, :visit_statement_node
 
       def add_simple_range(node)
         location = node.location
 
         if location.start_line < location.end_line
           add_range(location.start_line - 1, location.end_line - 1)
-        end
-      end
-
-      def add_statements_range(node)
-        unless node.statements.empty?
-          add_range(node.location.start_line - 1, node.statements.location.end_line - 1)
         end
       end
 
