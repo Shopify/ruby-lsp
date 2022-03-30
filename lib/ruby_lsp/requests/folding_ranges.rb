ab29fc0617e307a820d6b9a97eefb6b8c78fbcfb
Alexandre Terrasa <alexandre.terrasa@shopify.com>
2022-03-30 19:25:33 -0400

    Simplify imports blocks handling
    Signed-off-by: Alexandre Terrasa <alexandre.terrasa@shopify.com>

--- lib/ruby_lsp/requests/folding_ranges.rb
+++ lib/ruby_lsp/requests/folding_ranges.rb
@@ -17,7 +17,6 @@ module RubyLsp
 
       def run
         visit(@parsed_tree.tree)
-        emit_partial_range
         @ranges.filter! { |range| range.end_line > range.start_line } || @ranges
       end
 
@@ -62,80 +61,12 @@ module RubyLsp
           add_def_range(node)
           visit(node.bodystmt)
         when SyntaxTree::Command
-          add_node_range(node)
-          visit_all(node.child_nodes) if handle_partial_range(node)
+          add_import_range(node)
         when SyntaxTree::Comment
           add_comment_range(node)
         else
-          super if handle_partial_range(node)
+          visit_all(node.child_nodes)
         end
-      end
-
-      class PartialRange
-        attr_reader :kind, :end_line
-
-        def self.from(node, kind)
-          new(node.location.start_line - 1, node.location.end_line - 1, kind)
-        end
-
-        def initialize(start_line, end_line, kind)
-          @start_line = start_line
-          @end_line = end_line
-          @kind = kind
-        end
-
-        def extend_to(node)
-          @end_line = node.location.end_line - 1
-          self
-        end
-
-        def new_section?(node)
-          node.is_a?(SyntaxTree::Comment) && @end_line + 1 != node.location.start_line - 1
-        end
-
-        def to_range
-          LanguageServer::Protocol::Interface::FoldingRange.new(
-            start_line: @start_line,
-            end_line: @end_line,
-            kind: @kind
-          )
-        end
-      end
-
-      def handle_partial_range(node)
-        kind = partial_range_kind(node)
-
-        if kind.nil?
-          emit_partial_range
-          return true
-        end
-
-        @partial_range = if @partial_range.nil?
-          PartialRange.from(node, kind)
-        elsif @partial_range.kind != kind || @partial_range.new_section?(node)
-          emit_partial_range
-          PartialRange.from(node, kind)
-        else
-          @partial_range.extend_to(node)
-        end
-
-        false
-      end
-
-      def partial_range_kind(node)
-        case node
-        when SyntaxTree::Command
-          if node.message.value == "require" || node.message.value == "require_relative"
-            "imports"
-          end
-        end
-      end
-
-      def emit_partial_range
-        return if @partial_range.nil?
-
-        @ranges << @partial_range.to_range
-        @partial_range = nil
       end
 
       def add_statements_range(node, statements)
@@ -160,6 +91,20 @@ module RubyLsp
           extend_last_range_to_location(node.location)
         else
           add_node_range(node, kind: "comment")
+        end
+      end
+
+      def add_import_range(node)
+        if node.message.value != "require" && node.message.value != "require_relative"
+          add_node_range(node)
+          return
+        end
+
+        last_range = @ranges.last
+        if last_range&.kind == "imports"
+          extend_last_range_to_location(node.location)
+        else
+          add_node_range(node, kind: "imports")
         end
       end
 
