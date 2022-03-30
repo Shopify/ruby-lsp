f7ab222e30ac0183d98812d912b7c3a9cb1e0fcd
Vinicius Stock <vinistock@users.noreply.github.com>
2022-03-30 10:01:56 -0400

    Fold comments and requires (#22)
    * Add support for folding comments and requires
    
    * Extract partial_range_kind into a method
    
    * Refactor partial range logic into a class

--- lib/ruby_lsp/requests/folding_ranges.rb
+++ lib/ruby_lsp/requests/folding_ranges.rb
@@ -27,12 +27,14 @@ module RubyLsp
       def initialize(parsed_tree)
         @parsed_tree = parsed_tree
         @ranges = []
+        @partial_range = nil
 
         super()
       end
 
       def run
         visit(@parsed_tree.tree)
+        emit_partial_range
         @ranges
       end
 
@@ -46,6 +48,10 @@ module RubyLsp
             super
           end
         RUBY
+      end
+
+      def visit(node)
+        super if handle_partial_range(node)
       end
 
       def visit_arg_paren(node)
@@ -90,6 +96,71 @@ module RubyLsp
       alias_method :visit_when, :visit_statement_node
       alias_method :visit_ensure, :visit_statement_node
       alias_method :visit_rescue, :visit_statement_node
+
+      class PartialRange
+        attr_reader :kind
+
+        def self.from(node, kind)
+          new(node.location.start_line - 1, node.location.end_line - 1, kind)
+        end
+
+        def initialize(start_line, end_line, kind)
+          @start_line = start_line
+          @end_line = end_line
+          @kind = kind
+        end
+
+        def extend_to(node)
+          @end_line = node.location.end_line - 1
+          self
+        end
+
+        def to_range
+          LanguageServer::Protocol::Interface::FoldingRange.new(
+            start_line: @start_line,
+            end_line: @end_line,
+            kind: @kind
+          )
+        end
+      end
+
+      def handle_partial_range(node)
+        kind = partial_range_kind(node)
+
+        if kind.nil?
+          emit_partial_range
+          return true
+        end
+
+        @partial_range = if @partial_range.nil?
+          PartialRange.from(node, kind)
+        elsif @partial_range.kind != kind
+          emit_partial_range
+          PartialRange.from(node, kind)
+        else
+          @partial_range.extend_to(node)
+        end
+
+        false
+      end
+
+      def partial_range_kind(node)
+        case node
+        when SyntaxTree::Comment
+          "comment"
+        when SyntaxTree::Command
+          if node.message.value == "require" || node.message.value == "require_relative"
+            "imports"
+          end
+        end
+      end
+
+      def emit_partial_range
+        return if @partial_range.nil?
+
+        @ranges << @partial_range.to_range
+        @partial_range = nil
+      end
 
       def add_simple_range(node)
         location = node.location
