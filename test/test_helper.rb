fe0ee12116960b2d88dd70ac2643a5ff5ff67955
Vinicius Stock <vinistock@users.noreply.github.com>
2022-03-30 13:17:15 -0400

    Add debug gem (#28)

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -9,6 +9,7 @@ require "ruby_lsp/visitor"
 require "minitest/autorun"
 require "minitest/reporters"
 require "tempfile"
+require "debug"
 
 Minitest::Reporters.use!(Minitest::Reporters::SpecReporter.new(color: true))
 
