38318be66e5a33407991a1958072282a0a1d981c
Vinicius Stock <vinicius.stock@shopify.com>
2022-04-22 09:19:46 -0400

    Add LSP integration tests

---
+++ test/integration_test.rb
@@ -0,0 +1,123 @@
+# frozen_string_literal: true
+
+require "test_helper"
+require "open3"
+
+# Important integration test notes
+#
+# 1. If the request returns `nil`, use `send_request` and do not try to read the response or else we hang reading from
+# stdout
+# 2. Make sure the request name is exactly the expected in CLI (e.g.: textDocument/foldingRange instead of
+# textDocument/foldingRanges). If the name is incorrect, the LSP won't return anything and the test will hang trying to
+# read from stdout
+# 3. The goal is to verify that all parts are working together. Don't create extensive tests with long code examples -
+# those are meant for unit tests
+class IntegrationTest < Minitest::Test
+  def setup
+    # Start a new Ruby LSP server in a separate process and set the IOs to binary mode
+    @stdin, @stdout, @stderr, @wait_thr = Open3.popen3("bundle exec ruby-lsp")
+    @stdin.binmode
+    @stdout.binmode
+    @stderr.binmode
+
+    # Initialize the LSP
+    make_request(
+      "initialize",
+      {
+        initializationOptions: {
+          enabledFeatures: [
+            "documentSymbols",
+            "foldingRanges",
+            "semanticHighlighting",
+            "formatting",
+            "codeActions",
+          ],
+        },
+      }
+    )
+
+    # Open a document to serve as based for all requests
+    make_request("textDocument/didOpen", { textDocument: { uri: "file://#{__FILE__}", text: "class Foo\nend" } })
+  end
+
+  def teardown
+    # Tell the LSP to shutdown
+    make_request("shutdown")
+
+    # Make sure IOs are closed
+    @stdin.close
+    @stdout.close
+    @stderr.close
+
+    # Make sure the exit status is zero
+    assert_equal(0, @wait_thr.value)
+  end
+
+  def test_document_symbol
+    response = make_request("textDocument/documentSymbol", { textDocument: { uri: "file://#{__FILE__}" } })
+    symbol = response.first
+    assert_equal("Foo", symbol[:name])
+    assert_equal(RubyLsp::Requests::DocumentSymbol::SYMBOL_KIND[:class], symbol[:kind])
+  end
+
+  def test_semantic_highlighting
+    response = make_request("textDocument/semanticTokens/full", { textDocument: { uri: "file://#{__FILE__}" } })
+    assert_empty(response[:data])
+  end
+
+  def test_formatting
+    response = make_request("textDocument/formatting", { textDocument: { uri: "file://#{__FILE__}" } })
+    assert_equal(<<~FORMATTED, response.first[:newText])
+      # frozen_string_literal: true
+
+      class Foo
+      end
+    FORMATTED
+  end
+
+  def test_code_actions
+    response = make_request("textDocument/codeAction",
+      { textDocument: { uri: "file://#{__FILE__}" }, range: { start: { line: 0 }, end: { line: 1 } } })
+    quickfix = response.first
+    assert_equal("quickfix", quickfix[:kind])
+    assert_match(%r{Autocorrect .*/.*}, quickfix[:title])
+  end
+
+  def test_document_did_close
+    assert(send_request("textDocument/didClose", { textDocument: { uri: "file://#{__FILE__}" } }))
+  end
+
+  def test_document_did_change
+    assert(send_request(
+      "textDocument/didChange",
+      {
+        textDocument: { uri: "file://#{__FILE__}" },
+        contentChanges: [{ text: "class Foo\ndef bar\nend\nend" }],
+      }
+    ))
+  end
+
+  def test_folding_ranges
+    response = make_request("textDocument/foldingRange", { textDocument: { uri: "file://#{__FILE__}" } })
+    assert_equal({ startLine: 0, endLine: 1, kind: "region" }, response.first)
+  end
+
+  private
+
+  def make_request(request, params = nil)
+    send_request(request, params)
+
+    # Read headers until line breaks
+    headers = @stdout.gets("\r\n\r\n")
+
+    # Read the response content based on the length received in the headers
+    raw_response = @stdout.read(headers[/Content-Length: (\d+)/i, 1].to_i)
+    JSON.parse(raw_response, symbolize_names: true)[:result]
+  end
+
+  def send_request(request, params = nil)
+    params = params ? ",\"params\":#{params.to_json}" : ""
+    json = "{\"jsonrpc\":\"2.0\",\"id\":#{rand(100)},\"method\":\"#{request}\"#{params}}"
+    @stdin.write("Content-Length: #{json.length}\r\n\r\n#{json}")
+  end
+end
