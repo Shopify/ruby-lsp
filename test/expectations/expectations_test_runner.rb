6f1be8794a1d836764757aaaef34d1f8b837752b
Alexandre Terrasa <alexandre.terrasa@shopify.com>
2022-05-03 17:23:53 -0400

    Add a TODO to not forget
    Signed-off-by: Alexandre Terrasa <alexandre.terrasa@shopify.com>
    Co-authored-by: Vinicius Stock <vinistock@users.noreply.github.com>

--- test/expectations/expectations_test_runner.rb
+++ test/expectations/expectations_test_runner.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+# TODO: how to pass arguments to the test runner? See for example `CodeActionsTest`
+
 class ExpectationsTestRunner < Minitest::Test
   TEST_EXP_DIR = "test/expectations"
   TEST_FIXTURES_DIR = "test/fixtures"
