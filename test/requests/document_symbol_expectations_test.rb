0d0335a2d734de1112e3930832e3523d7bde5e1c
Alexandre Terrasa <alexandre.terrasa@shopify.com>
2022-05-03 17:37:50 -0400

    Rename DocumentationSymbolExpectationsTest into DocumentSymbolExpectationsTest
    Signed-off-by: Alexandre Terrasa <alexandre.terrasa@shopify.com>
    Co-authored-by: Vinicius Stock <vinistock@users.noreply.github.com>

--- test/requests/document_symbol_expectations_test.rb
+++ test/requests/document_symbol_expectations_test.rb
@@ -3,6 +3,6 @@
 require "test_helper"
 require "expectations/expectations_test_runner"
 
-class DocumentationSymbolExpectationsTest < ExpectationsTestRunner
+class DocumentSymbolExpectationsTest < ExpectationsTestRunner
   expectations_tests RubyLsp::Requests::DocumentSymbol, "document_symbol"
 end
