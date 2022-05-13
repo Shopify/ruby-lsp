7bc00f27c72e126f26514c0a16ed109d0816f108
Maple Ong <maple.ong@shopify.com>
2022-05-12 20:30:05 -0400

    Refactor #compute_delta and remove exception handling

--- lib/ruby_lsp/encoder/relative.rb
+++ lib/ruby_lsp/encoder/relative.rb
@@ -17,11 +17,8 @@ module RubyLsp
       def encode(tokens)
         tokens = sort(tokens)
 
-        delta = []
-        tokens.each do |token|
-          compute_delta(token) do |delta_line, delta_column|
-            delta.push(delta_line, delta_column, token.length, TOKEN_TYPES.index(token.classification), 0)
-          end
+        delta = tokens.flat_map do |token|
+          compute_delta(token)
         end
 
         LanguageServer::Protocol::Interface::SemanticTokens.new(data: delta)
@@ -40,23 +37,16 @@ module RubyLsp
         row = token.location.start_line - 1
         column = token.location.start_column
 
-        if row < @current_row
-          raise InvalidTokenRowError, "Invalid token row detected: " \
-            "Ensure tokens are added in the expected order."
-        end
-
         delta_line = row - @current_row
 
         delta_column = column
         delta_column -= @current_column if delta_line == 0
 
-        yield delta_line, delta_column
-
+        [delta_line, delta_column, token.length, TOKEN_TYPES.index(token.classification), 0]
+      ensure
         @current_row = row
         @current_column = column
       end
-
-      class InvalidTokenRowError < StandardError; end
 
       def sort(tokens)
         tokens.sort_by { |token| token.location.start_column }
