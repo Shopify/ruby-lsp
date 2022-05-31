586eccfa5f6de1640a58b9c5f4d364db6a314aca
Vinicius Stock <vinicius.stock@shopify.com>
2022-05-31 16:17:31 -0400

    Move requires to lib/internal

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,10 +3,7 @@
 
 $LOAD_PATH.unshift(File.expand_path("../lib", __dir__))
 
-require "sorbet-runtime"
-require "ruby-lsp"
-require "syntax_tree"
-require "ruby_lsp/cli"
+require_relative "../lib/internal"
 require "minitest/autorun"
 require "minitest/reporters"
 require "tempfile"
