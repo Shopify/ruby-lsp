dce80ac9c85cae407a729c88744b8ba77b5059de
Vinicius Stock <vinicius.stock@shopify.com>
2022-05-31 14:01:14 -0400

    Tune the Tapioca require file

--- sorbet/tapioca/require.rb
+++ sorbet/tapioca/require.rb
@@ -2,3 +2,6 @@
 # frozen_string_literal: true
 
 # Add your extra requires here (`bin/tapioca require` can be used to boostrap this list)
+
+require "language_server-protocol"
+require "syntax_tree"
