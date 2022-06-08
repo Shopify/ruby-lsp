419a338fb5abd3b6ab6714e96e85f41d59eb44f7
Maple Ong <maple.ong@shopify.com>
2022-06-08 09:23:21 -0400

    Add semantic token for def endless method name

--- lib/ruby_lsp/requests/semantic_highlighting.rb
+++ lib/ruby_lsp/requests/semantic_highlighting.rb
@@ -58,6 +58,13 @@ module RubyLsp
         visit(node.bodystmt)
       end
 
+      def visit_def_endless(node)
+        add_token(node.name.location, :method, [:declaration])
+        visit(node.paren)
+        visit(node.operator)
+        visit(node.statement)
+      end
+
       def visit_m_assign(node)
         node.target.parts.each do |var_ref|
           add_token(var_ref.value.location, :variable)
