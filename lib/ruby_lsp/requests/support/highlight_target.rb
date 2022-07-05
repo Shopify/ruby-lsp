7829e1c9ea60554bd943e1ae9aad8015ed3739c4
Vinicius Stock <vinicius.stock@shopify.com>
2022-07-05 15:46:17 -0400

    Add support for module, class and method highlight

--- lib/ruby_lsp/requests/support/highlight_target.rb
+++ lib/ruby_lsp/requests/support/highlight_target.rb
@@ -10,38 +10,48 @@ module RubyLsp
         READ = LanguageServer::Protocol::Constant::DocumentHighlightKind::READ
         WRITE = LanguageServer::Protocol::Constant::DocumentHighlightKind::WRITE
 
+        class HighlightMatch < T::Struct
+          const :type, Integer
+          const :node, SyntaxTree::Node
+        end
+
         sig { params(node: SyntaxTree::Node).void }
         def initialize(node)
           @node = node
           @value = T.let(value(node), T.nilable(String))
         end
 
-        sig { params(other: SyntaxTree::Node).returns(T.nilable(Integer)) }
+        sig { params(other: SyntaxTree::Node).returns(T.nilable(HighlightMatch)) }
         def highlight_type(other)
           matched_highlight(other) if @value == value(other)
         end
 
         private
 
-        sig { params(other: SyntaxTree::Node).returns(T.nilable(Integer)) }
+        # Match the target type (where the cursor is positioned) with the `other` type (the node we're currently
+        # visiting)
+        sig { params(other: SyntaxTree::Node).returns(T.nilable(HighlightMatch)) }
         def matched_highlight(other)
           case @node
-          when SyntaxTree::VCall
-            if other.is_a?(SyntaxTree::VCall)
-              READ
-            elsif other.is_a?(SyntaxTree::Def)
-              WRITE
+          when SyntaxTree::VCall, SyntaxTree::FCall, SyntaxTree::Call, SyntaxTree::Command,
+               SyntaxTree::CommandCall, SyntaxTree::Def, SyntaxTree::Defs, SyntaxTree::DefEndless
+            case other
+            when SyntaxTree::VCall, SyntaxTree::FCall, SyntaxTree::Call, SyntaxTree::Command, SyntaxTree::CommandCall
+              HighlightMatch.new(type: READ, node: other)
+            when SyntaxTree::Def, SyntaxTree::Defs, SyntaxTree::Defs
+              HighlightMatch.new(type: WRITE, node: other.name)
             end
-          when SyntaxTree::GVar,
-               SyntaxTree::IVar,
-               SyntaxTree::Const,
-               SyntaxTree::CVar,
-               SyntaxTree::VarField,
+          when SyntaxTree::GVar, SyntaxTree::IVar, SyntaxTree::Const, SyntaxTree::CVar, SyntaxTree::VarField,
                SyntaxTree::VarRef
-            if other.is_a?(SyntaxTree::VarField)
-              WRITE
-            elsif other.is_a?(SyntaxTree::VarRef)
-              READ
+            case other
+            when SyntaxTree::VarField
+              HighlightMatch.new(type: WRITE, node: other)
+            when SyntaxTree::VarRef
+              HighlightMatch.new(type: READ, node: other)
+            when SyntaxTree::ClassDeclaration, SyntaxTree::ModuleDeclaration
+              HighlightMatch.new(type: WRITE, node: other.constant)
+            when SyntaxTree::ConstPathRef
+              HighlightMatch.new(type: READ, node: other.constant)
             end
           end
         end
@@ -49,18 +59,18 @@ module RubyLsp
         sig { params(node: SyntaxTree::Node).returns(T.nilable(String)) }
         def value(node)
           case node
-          when SyntaxTree::GVar, SyntaxTree::IVar, SyntaxTree::Const, SyntaxTree::CVar
-            node.value
-          when SyntaxTree::Assign
-            value(node.target)
-          when SyntaxTree::ConstPathField, SyntaxTree::TopConstField
+          when SyntaxTree::ConstPathRef, SyntaxTree::ConstPathField, SyntaxTree::TopConstField
             node.constant.value
-          when SyntaxTree::Field
-            node.name.value
-          when SyntaxTree::VarField, SyntaxTree::VarRef
-            node.value&.value
-          when SyntaxTree::Ident
+          when SyntaxTree::GVar, SyntaxTree::IVar, SyntaxTree::Const, SyntaxTree::CVar, SyntaxTree::Ident
             node.value
+          when SyntaxTree::Field, SyntaxTree::Def, SyntaxTree::Defs, SyntaxTree::DefEndless
+            node.name.value
+          when SyntaxTree::VarField, SyntaxTree::VarRef, SyntaxTree::VCall, SyntaxTree::FCall
+            node.value&.value
+          when SyntaxTree::Call, SyntaxTree::Command, SyntaxTree::CommandCall
+            node.message.value
+          when SyntaxTree::ClassDeclaration, SyntaxTree::ModuleDeclaration
+            node.constant.constant.value
           end
         end
       end
