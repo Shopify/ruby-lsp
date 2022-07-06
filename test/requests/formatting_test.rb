68b2d243e62bdb38f3d93b30cb4dfb274d208853
Vinicius Stock <vinicius.stock@shopify.com>
2022-07-06 15:42:26 -0400

    Extract RuboCop disabling logic into a method

--- test/requests/formatting_test.rb
+++ test/requests/formatting_test.rb
@@ -26,26 +26,32 @@ class FormattingTest < Minitest::Test
   end
 
   def test_formats_with_syntax_tree_when_rubocop_is_not_present
+    with_disabled_rubocop do
+      assert_equal(<<~RUBY, formatted_document)
+        class Foo
+          def foo
+          end
+        end
+      RUBY
+    end
+  end
+
+  private
+
+  def with_disabled_rubocop(&block)
     rubocop_paths = $LOAD_PATH.select { |path| path.include?("gems/rubocop") }
     rubocop_paths.each { |path| $LOAD_PATH.delete(path) }
     $LOADED_FEATURES.delete_if { |path| path.include?("ruby_lsp/requests") || path.include?("gems/rubocop") }
     RubyLsp.send(:remove_const, :Requests)
     Object.send(:remove_const, :RuboCop)
 
-    assert_equal(<<~RUBY, formatted_document)
-      class Foo
-        def foo
-        end
-      end
-    RUBY
+    block.call
   ensure
     $LOAD_PATH.unshift(*rubocop_paths)
     $LOADED_FEATURES.delete_if { |path| path.include?("ruby_lsp/requests") }
     RubyLsp.send(:remove_const, :Requests)
     require "ruby_lsp/requests"
   end
-
-  private
 
   def formatted_document
     require "ruby_lsp/requests"
