9ec04956f170a5104d4a7cc0c0392d5dc9d6d742
Stan Lo <stan.lo@shopify.com>
2022-07-26 20:52:09 +0100

    Add documentation for documentLink

--- lib/ruby_lsp/requests/document_link.rb
+++ lib/ruby_lsp/requests/document_link.rb
@@ -3,6 +3,19 @@
 
 module RubyLsp
   module Requests
+    # ![Document link demo](../../misc/document_link.gif)
+    #
+    # The [document link](https://microsoft.github.io/language-server-protocol/specification#textDocument_documentLink)
+    # makes `# source://PATH_TO_FILE:line` comments in a Ruby/RBI file clickable if the file exists.
+    # When the user clicks the link, it'll take the user to that location.
+    #
+    # # Example
+    #
+    # ```ruby
+    # # source://syntax_tree-3.2.1/lib/syntax_tree.rb:51 <- it will be clickable and will take the user to that location
+    # def format(source, maxwidth = T.unsafe(nil))
+    # end
+    # ```
     class DocumentLink < BaseRequest
       extend T::Sig
 
