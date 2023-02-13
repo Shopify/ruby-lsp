94afff3b43e0537f86bb0c5a76291c4d73c64c33
Andy Waite <andyw8@users.noreply.github.com>
2023-02-13 13:37:00 -0500

    Wrap env JSON in delimiter

--- src/ruby.ts
+++ src/ruby.ts
@@ -63,12 +63,15 @@ export class Ruby {
   private async activate(ruby: string) {
     const result = await asyncExec(
       // eslint-disable-next-line no-process-env
-      `${process.env.SHELL} -lic '${ruby} --disable-gems -rjson -e "puts JSON.dump(ENV.to_h)"'`,
+      `${process.env.SHELL} -lic '${ruby} --disable-gems -rjson -e "puts 'RUBY_ENV_ACTIVATE' + JSON.dump(ENV.to_h) + 'RUBY_ENV_ACTIVATE'"'`,
       { cwd: this.workingFolder }
     );
 
+    const envJson = result.stdout.match(
+      /RUBY_ENV_ACTIVATE(.*)RUBY_ENV_ACTIVATE/
+    )![1];
     // eslint-disable-next-line no-process-env
-    process.env = JSON.parse(result.stdout);
+    process.env = JSON.parse(envJson);
   }
 
   private async fetchRubyInfo() {
