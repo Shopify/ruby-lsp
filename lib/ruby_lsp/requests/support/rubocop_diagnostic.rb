e2997f78a2b914d6789c14a37429c51cd37135e0
Vinicius Stock <vinicius.stock@shopify.com>
2023-02-15 10:36:18 -0500

    Use aliased LSP interface and constants in RuboCopDiagnostic

--- lib/ruby_lsp/requests/support/rubocop_diagnostic.rb
+++ lib/ruby_lsp/requests/support/rubocop_diagnostic.rb
@@ -9,12 +9,12 @@ module RubyLsp
 
         RUBOCOP_TO_LSP_SEVERITY = T.let(
           {
-            convention: LanguageServer::Protocol::Constant::DiagnosticSeverity::INFORMATION,
-            info: LanguageServer::Protocol::Constant::DiagnosticSeverity::INFORMATION,
-            refactor: LanguageServer::Protocol::Constant::DiagnosticSeverity::INFORMATION,
-            warning: LanguageServer::Protocol::Constant::DiagnosticSeverity::WARNING,
-            error: LanguageServer::Protocol::Constant::DiagnosticSeverity::ERROR,
-            fatal: LanguageServer::Protocol::Constant::DiagnosticSeverity::ERROR,
+            convention: Constant::DiagnosticSeverity::INFORMATION,
+            info: Constant::DiagnosticSeverity::INFORMATION,
+            refactor: Constant::DiagnosticSeverity::INFORMATION,
+            warning: Constant::DiagnosticSeverity::WARNING,
+            error: Constant::DiagnosticSeverity::ERROR,
+            fatal: Constant::DiagnosticSeverity::ERROR,
           }.freeze,
           T::Hash[Symbol, Integer],
         )
@@ -27,13 +27,13 @@ module RubyLsp
 
         sig { returns(Interface::CodeAction) }
         def to_lsp_code_action
-          LanguageServer::Protocol::Interface::CodeAction.new(
+          Interface::CodeAction.new(
             title: "Autocorrect #{@offense.cop_name}",
-            kind: LanguageServer::Protocol::Constant::CodeActionKind::QUICK_FIX,
-            edit: LanguageServer::Protocol::Interface::WorkspaceEdit.new(
+            kind: Constant::CodeActionKind::QUICK_FIX,
+            edit: Interface::WorkspaceEdit.new(
               document_changes: [
-                LanguageServer::Protocol::Interface::TextDocumentEdit.new(
-                  text_document: LanguageServer::Protocol::Interface::OptionalVersionedTextDocumentIdentifier.new(
+                Interface::TextDocumentEdit.new(
+                  text_document: Interface::OptionalVersionedTextDocumentIdentifier.new(
                     uri: @uri,
                     version: nil,
                   ),
@@ -45,26 +45,27 @@ module RubyLsp
           )
         end
 
-        sig { returns(LanguageServer::Protocol::Interface::Diagnostic) }
+        sig { returns(Interface::Diagnostic) }
         def to_lsp_diagnostic
           if @offense.correctable?
             severity = RUBOCOP_TO_LSP_SEVERITY[@offense.severity.name]
             message = @offense.message
           else
-            severity = LanguageServer::Protocol::Constant::DiagnosticSeverity::WARNING
+            severity = Constant::DiagnosticSeverity::WARNING
             message = "#{@offense.message}\n\nThis offense is not auto-correctable.\n"
           end
-          LanguageServer::Protocol::Interface::Diagnostic.new(
+
+          Interface::Diagnostic.new(
             message: message,
             source: "RuboCop",
             code: @offense.cop_name,
             severity: severity,
-            range: LanguageServer::Protocol::Interface::Range.new(
-              start: LanguageServer::Protocol::Interface::Position.new(
+            range: Interface::Range.new(
+              start: Interface::Position.new(
                 line: @offense.line - 1,
                 character: @offense.column,
               ),
-              end: LanguageServer::Protocol::Interface::Position.new(
+              end: Interface::Position.new(
                 line: @offense.last_line - 1,
                 character: @offense.last_column,
               ),
@@ -78,16 +79,13 @@ module RubyLsp
 
         private
 
-        sig { returns(T::Array[LanguageServer::Protocol::Interface::TextEdit]) }
+        sig { returns(T::Array[Interface::TextEdit]) }
         def offense_replacements
           @offense.corrector.as_replacements.map do |range, replacement|
-            LanguageServer::Protocol::Interface::TextEdit.new(
-              range: LanguageServer::Protocol::Interface::Range.new(
-                start: LanguageServer::Protocol::Interface::Position.new(line: range.line - 1, character: range.column),
-                end: LanguageServer::Protocol::Interface::Position.new(
-                  line: range.last_line - 1,
-                  character: range.last_column,
-                ),
+            Interface::TextEdit.new(
+              range: Interface::Range.new(
+                start: Interface::Position.new(line: range.line - 1, character: range.column),
+                end: Interface::Position.new(line: range.last_line - 1, character: range.last_column),
               ),
               new_text: replacement,
             )
