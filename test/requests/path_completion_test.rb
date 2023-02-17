6b9b037f60fa7350e67fcbf017787d28bc91ff61
Ufuk Kayserilioglu <ufuk@paralaus.com>
2023-02-17 18:10:32 +0200

    Add test for completion requests with partial path

--- test/requests/path_completion_test.rb
+++ test/requests/path_completion_test.rb
@@ -88,6 +88,31 @@ class PathCompletionTest < Minitest::Test
     assert_equal(expected.to_json, result.to_json)
   end
 
+  def test_completion_with_partial_path
+    prefix = "foo/suppo"
+
+    document = RubyLsp::Document.new(+<<~RUBY)
+      require "#{prefix}"
+    RUBY
+
+    position = {
+      line: 0,
+      character: document.source.rindex('"') || 0,
+    }
+
+    result = with_file_structure do
+      RubyLsp::Requests::PathCompletion.new(document, position).run
+    end
+
+    expected = [
+      path_completion("foo/support/bar", prefix, position),
+      path_completion("foo/support/baz", prefix, position),
+      path_completion("foo/support/quux", prefix, position),
+    ]
+
+    assert_equal(expected.to_json, result.to_json)
+  end
+
   def test_completion_does_not_fail_when_there_are_syntax_errors
     document = RubyLsp::Document.new(+<<~RUBY)
       require "ruby_lsp/requests/"
