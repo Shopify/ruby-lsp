f34fc383a817404048e5baee161c49665c33ff21
Adison Lampert <adison.lampert@shopify.com>
2023-03-31 12:54:48 -0400

    Add code lens for test methods

--- lib/ruby_lsp/requests/code_lens.rb
+++ lib/ruby_lsp/requests/code_lens.rb
@@ -32,6 +32,7 @@ module RubyLsp
         super(document)
         @results = T.let([], T::Array[Interface::CodeLens])
         @path = T.let(document.uri.delete_prefix("file://"), String)
+        @modifier = T.let("public", String)
       end
 
       sig { override.returns(T.all(T::Array[Interface::CodeLens], Object)) }
@@ -44,19 +45,63 @@ module RubyLsp
       def visit_class(node)
         class_name = node.constant.constant.value
         if class_name.end_with?("Test")
-          add_code_lens(node, [@path, class_name, BASE_COMMAND + " -Itest " + @path])
+          add_code_lens(node, path: @path, name: class_name, command: BASE_COMMAND + " -Itest " + @path)
+        end
+        visit(node.bodystmt)
+      end
+
+      sig { override.params(node: SyntaxTree::DefNode).void }
+      def visit_def(node)
+        if @modifier == "public"
+          method_name = node.name.value
+          if method_name.start_with?("test")
+            add_code_lens(
+              node,
+              path: @path + ":" + node.location.start_line.to_s,
+              name: method_name,
+              command: BASE_COMMAND + " -Itest " + @path + " --name " + method_name,
+            )
+          end
+        end
+      end
+
+      sig { override.params(node: SyntaxTree::Command).void }
+      def visit_command(node)
+        if node.message.value == "public"
+          modifier = @modifier
+          @modifier = "public"
+          visit(node.arguments)
+          @modifier = modifier
+        end
+      end
+
+      sig { override.params(node: SyntaxTree::VCall).void }
+      def visit_vcall(node)
+        vcall_value = node.value.value
+
+        if ["private", "protected", "public"].include?(vcall_value)
+          @modifier = vcall_value
         end
       end
 
       private
 
-      sig { params(node: SyntaxTree::Node, args: T::Array[String]).void }
-      def add_code_lens(node, args)
+      sig { params(node: SyntaxTree::Node, path: String, name: String, command: String).void }
+      def add_code_lens(node, path:, name:, command:)
         @results << Interface::CodeLens.new(
           range: range_from_syntax_tree_node(node),
-          command: Interface::Command.new(title: "Run", command: "rubyLsp.runTest", arguments: args),
+          command: Interface::Command.new(title: "Run", command: "rubyLsp.runTest", arguments: [path, name, command]),
           data: { type: "test" },
         )
+      end
+
+      sig { returns(String) }
+      def test_command
+        if ENV["BUNDLE_GEMFILE"]
+          "bundle exec ruby -Itest "
+        else
+          "ruby -Itest "
+        end
       end
     end
   end
--- test/fixtures/minitest_tests.rb
+++ test/fixtures/minitest_tests.rb
@@ -1,11 +1,15 @@
 class Test < Minitest::Test
-  def test_method
-  end
+  private def test_private_command; end
 
-  def helper_method
-  end
+  def test_public; end
 
   private
-  def test_private
-  end
+
+  public def test_public_command; end
+
+  def test_private_vcall; end
+
+  public
+
+  def test_public_vcall; end
 end
