5cf8b06392a70b57fe6634af1ff130f7aed31e6d
Adison Lampert <adison.lampert@shopify.com>
2023-04-05 14:35:13 -0400

    Check for access modifiers using call visit

--- lib/ruby_lsp/requests/code_lens.rb
+++ lib/ruby_lsp/requests/code_lens.rb
@@ -21,7 +21,8 @@ module RubyLsp
     # ```
 
     class CodeLens < BaseRequest
-      BASE_COMMAND = T.let(File.exist?("Gemfile.lock") ? "bundle exec ruby" : "ruby", String)
+      BASE_COMMAND = T.let((File.exist?("Gemfile.lock") ? "bundle exec ruby" : "ruby") + " -Itest ", String)
+      ACCESS_MODIFIERS = T.let(["public", "private", "protected"], T::Array[String])
 
       sig do
         params(
@@ -45,7 +46,7 @@ module RubyLsp
       def visit_class(node)
         class_name = node.constant.constant.value
         if class_name.end_with?("Test")
-          add_code_lens(node, path: @path, name: class_name, command: BASE_COMMAND + " -Itest " + @path)
+          add_code_lens(node, name: class_name, command: BASE_COMMAND + @path)
         end
         visit(node.bodystmt)
       end
@@ -54,12 +55,11 @@ module RubyLsp
       def visit_def(node)
         if @modifier == "public"
           method_name = node.name.value
-          if method_name.start_with?("test")
+          if method_name.start_with?("test_")
             add_code_lens(
               node,
-              path: @path + ":" + node.location.start_line.to_s,
               name: method_name,
-              command: BASE_COMMAND + " -Itest " + @path + " --name " + method_name,
+              command: BASE_COMMAND + @path + " --name " + method_name,
             )
           end
         end
@@ -68,10 +68,18 @@ module RubyLsp
       sig { override.params(node: SyntaxTree::Command).void }
       def visit_command(node)
         if node.message.value == "public"
-          modifier = @modifier
-          @modifier = "public"
-          visit(node.arguments)
-          @modifier = modifier
+          with_visiblity("public", node)
+        end
+      end
+
+      sig { override.params(node: SyntaxTree::CallNode).void }
+      def visit_call(node)
+        ident = node.message if node.message.is_a?(SyntaxTree::Ident)
+
+        if ident
+          if T.cast(ident, SyntaxTree::Ident).value == "public"
+            with_visiblity("public", node)
+          end
         end
       end
 
@@ -79,18 +87,46 @@ module RubyLsp
       def visit_vcall(node)
         vcall_value = node.value.value
 
-        if ["private", "protected", "public"].include?(vcall_value)
+        if ACCESS_MODIFIERS.include?(vcall_value)
           @modifier = vcall_value
         end
       end
 
       private
 
-      sig { params(node: SyntaxTree::Node, path: String, name: String, command: String).void }
-      def add_code_lens(node, path:, name:, command:)
+      sig do
+        params(
+          visibility: String,
+          node: T.any(SyntaxTree::CallNode, SyntaxTree::Command),
+        ).void
+      end
+      def with_visiblity(visibility, node)
+        current_visibility = @modifier
+        @modifier = visibility
+        visit(node.arguments)
+      ensure
+        @modifier = T.must(current_visibility)
+      end
+
+      sig { params(node: SyntaxTree::Node, name: String, command: String).void }
+      def add_code_lens(node, name:, command:)
         @results << Interface::CodeLens.new(
           range: range_from_syntax_tree_node(node),
-          command: Interface::Command.new(title: "Run", command: "rubyLsp.runTest", arguments: [path, name, command]),
+          command: Interface::Command.new(
+            title: "Run",
+            command: "rubyLsp.runTest",
+            arguments: [
+              @path,
+              name,
+              command,
+              {
+                start_line: node.location.start_line - 1,
+                start_column: node.location.start_column,
+                end_line: node.location.end_line - 1,
+                end_column: node.location.end_column,
+              },
+            ],
+          ),
           data: { type: "test" },
         )
       end
--- test/fixtures/minitest_tests.rb
+++ test/fixtures/minitest_tests.rb
@@ -1,11 +1,15 @@
 class Test < Minitest::Test
   private def test_private_command; end
 
+  private(def test_another_private; end)
+
   def test_public; end
 
   private
 
   public def test_public_command; end
+
+  public(def test_another_public; end)
 
   def test_private_vcall; end
 
