4a5d5a285dfddd8f98e775a5b9610eef1793cfae
Andy Waite <andyw8@users.noreply.github.com>
2023-04-18 13:38:48 -0400

    Fix keyword recognition for on-type formatting

--- test/requests/on_type_formatting_test.rb
+++ test/requests/on_type_formatting_test.rb
@@ -119,6 +119,21 @@ class OnTypeFormattingTest < Minitest::Test
     assert_equal(expected_edits.to_json, T.must(edits).to_json)
   end
 
+  def test_keyword_handling
+    document = RubyLsp::Document.new(source: +"", version: 1, uri: "file:///fake.rb")
+
+    document.push_edits(
+      [{
+        range: { start: { line: 0, character: 0 }, end: { line: 0, character: 0 } },
+        text: "\"def\"g\"",
+      }],
+      version: 2,
+    )
+
+    edits = RubyLsp::Requests::OnTypeFormatting.new(document, { line: 0, character: 5 }, "\n").run
+    assert_empty(edits)
+  end
+
   def test_comment_continuation_with_other_line_break_matches
     document = RubyLsp::Document.new(source: +"", version: 1, uri: "file:///fake.rb")
 
