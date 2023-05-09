3088859edd1a661ee3c9337700cb56558d84eb33
snutij <justin.rabiller@doctolib.com>
2023-05-09 22:04:55 +0200

    feat: add openGem codelens

--- src/status.ts
+++ src/status.ts
@@ -24,6 +24,7 @@ export enum Command {
   RunTest = "rubyLsp.runTest",
   RunTestInTerminal = "rubyLsp.runTestInTerminal",
   DebugTest = "rubyLsp.debugTest",
+  OpenGem = "rubyLsp.openGem",
 }
 
 const STOPPED_SERVER_OPTIONS = [
