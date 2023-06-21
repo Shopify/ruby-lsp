90beee6951cdef5fdd362e28d95f0608faaaf01e
Stan Lo <stan.lo@shopify.com>
2023-06-21 22:43:32 +0100

    Refactor empty reponse assertions

--- test/requests/code_lens_expectations_test.rb
+++ test/requests/code_lens_expectations_test.rb
@@ -58,7 +58,7 @@ class CodeLensExpectationsTest < ExpectationsTestRunner
     emitter.visit(document.tree)
     response = listener.response
 
-    assert_equal(0, response.size)
+    assert_empty(response)
   end
 
   def test_no_code_lens_for_rspec
@@ -76,7 +76,7 @@ class CodeLensExpectationsTest < ExpectationsTestRunner
     emitter.visit(document.tree)
     response = listener.response
 
-    assert_equal(0, response.size)
+    assert_empty(response)
   end
 
   def test_after_request_hook
