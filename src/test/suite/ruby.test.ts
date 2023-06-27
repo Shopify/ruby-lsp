f901fe42c9fd8a6bb8a3cee3278e3bdddaac9267
Vinicius Stock <vinicius.stock@shopify.com>
2023-06-27 16:05:48 -0400

    Remove custom bundle logic

--- src/test/suite/ruby.test.ts
+++ src/test/suite/ruby.test.ts
@@ -25,12 +25,5 @@ suite("Ruby environment activation", () => {
       true,
       "Expected YJIT support to be enabled",
     );
-    assert.strictEqual(
-      ruby.env.BUNDLE_GEMFILE,
-      // eslint-disable-next-line no-process-env
-      `${process.env.PWD}/.ruby-lsp/Gemfile`,
-      "Expected BUNDLE_GEMFILE to be set",
-    );
-    assert.strictEqual(ruby.env.BUNDLE_PATH__SYSTEM, "true");
   });
 });
