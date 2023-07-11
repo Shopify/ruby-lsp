fd26c0cf061cffb87c2e8cc8a0c67d0b9164e4e6
Haroon Rasheed <haroonar32@gmail.com>
2023-07-12 03:01:01 +0500

    early return in case of gem being removed

--- lib/ruby_lsp/requests/document_link.rb
+++ lib/ruby_lsp/requests/document_link.rb
@@ -95,7 +95,9 @@ module RubyLsp
         return unless match
 
         uri = T.cast(URI(T.must(match[0])), URI::Source)
-        gem_version = T.must(resolve_version(uri))
+        gem_version = resolve_version(uri)
+        return if gem_version.nil?
+
         file_path = self.class.gem_paths.dig(uri.gem_name, gem_version, CGI.unescape(uri.path))
         return if file_path.nil?
 
