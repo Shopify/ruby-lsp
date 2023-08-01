39e2f2843879efd2f5ebc6f96ef7e04e3a496609
Andy Waite <andyw8@users.noreply.github.com>
2023-08-01 16:17:00 -0400

    WIP: highlight_target

--- lib/ruby_lsp/requests/support/highlight_target.rb
+++ lib/ruby_lsp/requests/support/highlight_target.rb
@@ -16,56 +16,56 @@ module RubyLsp
           sig { returns(Integer) }
           attr_reader :type
 
-          sig { returns(SyntaxTree::Node) }
+          sig { returns(YARP::Node) }
           attr_reader :node
 
-          sig { params(type: Integer, node: SyntaxTree::Node).void }
+          sig { params(type: Integer, node: YARP::Node).void }
           def initialize(type:, node:)
             @type = type
             @node = node
           end
         end
 
-        sig { params(node: SyntaxTree::Node).void }
+        sig { params(node: YARP::Node).void }
         def initialize(node)
           @node = node
           @value = T.let(value(node), T.nilable(String))
         end
 
-        sig { params(other: SyntaxTree::Node).returns(T.nilable(HighlightMatch)) }
+        sig { params(other: YARP::Node).returns(T.nilable(HighlightMatch)) }
         def highlight_type(other)
-          matched_highlight(other) if other.is_a?(SyntaxTree::Params) || (@value && @value == value(other))
+          matched_highlight(other) if other.is_a?(YARP::ParametersNode) || (@value && @value == value(other))
         end
 
         private
 
         # Match the target type (where the cursor is positioned) with the `other` type (the node we're currently
         # visiting)
-        sig { params(other: SyntaxTree::Node).returns(T.nilable(HighlightMatch)) }
+        sig { params(other: YARP::Node).returns(T.nilable(HighlightMatch)) }
         def matched_highlight(other)
           case @node
           # Method definitions and invocations
-          when SyntaxTree::VCall, SyntaxTree::CallNode, SyntaxTree::Command,
-               SyntaxTree::CommandCall, SyntaxTree::DefNode
+          when YARP::CallNode, YARP::DefNode
             case other
-            when SyntaxTree::VCall, SyntaxTree::CallNode, SyntaxTree::Command, SyntaxTree::CommandCall
+            when YARP::CallNode
               HighlightMatch.new(type: READ, node: other)
-            when SyntaxTree::DefNode
+            when YARP::DefNode
               HighlightMatch.new(type: WRITE, node: other.name)
             end
           # Variables, parameters and constants
-          when SyntaxTree::GVar, SyntaxTree::IVar, SyntaxTree::Const, SyntaxTree::CVar, SyntaxTree::VarField,
-               SyntaxTree::VarRef, SyntaxTree::Ident
+          when YARP::GlobalVariableReadNode, YARP::InstanceVariableReadNode, YARP::ConstantReadNode, YARP::ClassVariableReadNode, SyntaxTree::VarField,
+               # SyntaxTree::VarRef, SyntaxTree::Ident,
+               YARP::GlobalVariableReadNode
             case other
-            when SyntaxTree::VarField
-              HighlightMatch.new(type: WRITE, node: other)
-            when SyntaxTree::VarRef
-              HighlightMatch.new(type: READ, node: other)
-            when SyntaxTree::ClassDeclaration, SyntaxTree::ModuleDeclaration
+            # when SyntaxTree::VarField
+            #   HighlightMatch.new(type: WRITE, node: other)
+            # when SyntaxTree::VarRef
+            #   HighlightMatch.new(type: READ, node: other)
+            when YARP::ClassNode, YARP::ModuleNode
               HighlightMatch.new(type: WRITE, node: other.constant)
             when SyntaxTree::ConstPathRef
               HighlightMatch.new(type: READ, node: other.constant)
-            when SyntaxTree::Params
+            when YARP::ParametersNode
               params = other.child_nodes.compact
               match = params.find { |param| value(param) == @value }
               HighlightMatch.new(type: WRITE, node: match) if match
@@ -73,23 +73,23 @@ module RubyLsp
           end
         end
 
-        sig { params(node: SyntaxTree::Node).returns(T.nilable(String)) }
+        sig { params(node: YARP::Node).returns(T.nilable(String)) }
         def value(node)
           case node
-          when SyntaxTree::ConstPathRef, SyntaxTree::ConstPathField, SyntaxTree::TopConstField
+          when SyntaxTree::ConstPathRef, YARP::ConstantPathNode # , SyntaxTree::TopConstField
             node.constant.value
-          when SyntaxTree::GVar, SyntaxTree::IVar, SyntaxTree::Const, SyntaxTree::CVar, SyntaxTree::Ident
+          when SyntaxTree::GVar, SyntaxTree::IVar, SyntaxTree::Const, YARP::ClassVariableReadNode, SyntaxTree::Ident
             node.value
-          when SyntaxTree::Field, SyntaxTree::DefNode, SyntaxTree::RestParam,
-               SyntaxTree::KwRestParam, SyntaxTree::BlockArg
+          when SyntaxTree::Field, YARP::DefNode, SyntaxTree::RestParam,
+               SyntaxTree::KwRestParam, YARP::BlockArgumentNode
             node.name&.value
           when SyntaxTree::VarField, SyntaxTree::VarRef, SyntaxTree::VCall
             value = node.value
             value.value unless value.nil? || value.is_a?(Symbol)
-          when SyntaxTree::CallNode, SyntaxTree::Command, SyntaxTree::CommandCall
+          when YARP::CallNode
             message = node.message
             message.value unless message.is_a?(Symbol)
-          when SyntaxTree::ClassDeclaration, SyntaxTree::ModuleDeclaration
+          when YARP::ClassNode, YARP::ModuleNode,
             node.constant.constant.value
           end
         end
