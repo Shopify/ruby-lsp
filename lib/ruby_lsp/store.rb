428c909e1df31d2ea53c137300ef8c5c8190873e
Vinicius Stock <vinistock@users.noreply.github.com>
2023-08-17 10:38:09 -0400

    Always use complete URI for store key (#891)
    Always use complete URI as store key for documents

--- lib/ruby_lsp/store.rb
+++ lib/ruby_lsp/store.rb
@@ -30,25 +30,23 @@ module RubyLsp
 
     sig { params(uri: URI::Generic).returns(Document) }
     def get(uri)
-      path = uri.to_standardized_path
-      return T.must(@state[T.must(uri.opaque)]) unless path
-
-      document = @state[path]
+      document = @state[uri.to_s]
       return document unless document.nil?
 
-      set(uri: uri, source: File.binread(CGI.unescape(path)), version: 0)
-      T.must(@state[path])
+      path = T.must(uri.to_standardized_path)
+      set(uri: uri, source: File.binread(path), version: 0)
+      T.must(@state[uri.to_s])
     end
 
     sig { params(uri: URI::Generic, source: String, version: Integer).void }
     def set(uri:, source:, version:)
       document = Document.new(source: source, version: version, uri: uri, encoding: @encoding)
-      @state[uri.storage_key] = document
+      @state[uri.to_s] = document
     end
 
     sig { params(uri: URI::Generic, edits: T::Array[Document::EditShape], version: Integer).void }
     def push_edits(uri:, edits:, version:)
-      T.must(@state[uri.storage_key]).push_edits(edits, version: version)
+      T.must(@state[uri.to_s]).push_edits(edits, version: version)
     end
 
     sig { void }
@@ -63,7 +61,7 @@ module RubyLsp
 
     sig { params(uri: URI::Generic).void }
     def delete(uri)
-      @state.delete(uri.storage_key)
+      @state.delete(uri.to_s)
     end
 
     sig do
