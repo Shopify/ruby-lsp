9a7592e1e58688750b51b5e64a92133236e3a0df
Andy Waite <andyw8@users.noreply.github.com>
2023-08-17 15:12:09 -0400

    Use consistent ordering in expectations

--- verify_expectations_format.rb
+++ verify_expectations_format.rb
@@ -1,10 +1,14 @@
-require 'json'
+# typed: strict
+# frozen_string_literal: true
+
+require "json"
 
 Dir["test/expectations/**/*.json"].each do |file|
   json = JSON.parse(File.read(file))
   params_index = json.keys.index("params")
   result_index = json.keys.index("result")
   if params_index && params_index > result_index
-    exit("Params should be before result in #{file}")
+    puts "params should be before result in #{file}"
+    exit(1)
   end
 end
