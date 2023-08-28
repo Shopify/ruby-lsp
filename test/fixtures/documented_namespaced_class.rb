e4f09672d7f64ba6fd8c717e881fbe5ce473ae79
Andy Waite <13400+andyw8@users.noreply.github.com>
2023-08-28 15:57:55 -0400

    Add expectation for namespaced hover (#941)
    Co-authored-by: Andy Waite <andyw8@users.noreply.github.com>

---
+++ test/fixtures/documented_namespaced_class.rb
@@ -0,0 +1,13 @@
+# This is the documentation for Foo::Bar
+class Foo::Bar
+end
+
+# This is more documentation for Foo::Bar
+class Foo::Bar
+end
+
+class Foo
+  # This is even more documentation for Foo::Bar
+  class Bar
+  end
+end
