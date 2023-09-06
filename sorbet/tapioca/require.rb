df2109fd4e9f12ffc4a8d22ec04659a6ac55b697
Andy Waite <andyw8@users.noreply.github.com>
2023-09-06 10:01:57 -0400

    Preparation for YARP migration

--- sorbet/tapioca/require.rb
+++ sorbet/tapioca/require.rb
@@ -4,8 +4,7 @@
 # Add your extra requires here (`bin/tapioca require` can be used to bootstrap this list)
 
 require "language_server-protocol"
-require "syntax_tree"
-require "syntax_tree/cli"
+require "yarp"
 require "mocha/minitest"
 require "rubocop/minitest/assert_offense"
 require "yarp"
