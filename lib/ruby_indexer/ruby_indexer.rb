b0e8559d4d9646d1ce58911fbb612b9b7a585469
Andy Waite <andyw8@users.noreply.github.com>
2023-10-17 13:12:55 -0400

    Extract Entry from Indexer

--- lib/ruby_indexer/ruby_indexer.rb
+++ lib/ruby_indexer/ruby_indexer.rb
@@ -7,6 +7,7 @@ require "did_you_mean"
 require "ruby_indexer/lib/ruby_indexer/indexable_path"
 require "ruby_indexer/lib/ruby_indexer/visitor"
 require "ruby_indexer/lib/ruby_indexer/index"
+require "ruby_indexer/lib/ruby_indexer/index/entry"
 require "ruby_indexer/lib/ruby_indexer/configuration"
 require "ruby_indexer/lib/ruby_indexer/prefix_tree"
 
