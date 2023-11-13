43313fe5928332f0ca3c602174762ebe2ab9018e
Marco Roth <marco.roth@intergga.ch>
2023-11-13 14:55:12 -0800

    Add `RubyLsp::ERBDocument#scan`

--- lib/ruby_lsp/erb_document.rb
+++ lib/ruby_lsp/erb_document.rb
@@ -1,6 +1,8 @@
 # typed: strict
 # frozen_string_literal: true
 
+require "strscan"
+
 module RubyLsp
   class ERBDocument < Document
     sig { override.returns(Prism::ParseResult) }
@@ -8,8 +10,33 @@ module RubyLsp
       return @parse_result unless @needs_parsing
 
       @needs_parsing = false
-      erb = ERB.new(@source)
-      @parse_result = Prism.parse(erb.src)
+
+      @parse_result = Prism.parse(scan(@source))
+    end
+
+    private
+
+    sig { params(source: String).returns(String) }
+    def scan(source)
+      scanner = StringScanner.new(source)
+      output = +""
+
+      until scanner.eos?
+        non_ruby_code = scanner.scan_until(/<%(-|=)?/)
+        break unless non_ruby_code
+
+        output << non_ruby_code.gsub(/[^\n]/, " ")
+
+        ruby_code = scanner.scan_until(/(-)?%>/)
+        break unless ruby_code
+
+        output << ruby_code[...-2]
+        output << "  "
+      end
+
+      warn(output)
+
+      output
     end
   end
 end
