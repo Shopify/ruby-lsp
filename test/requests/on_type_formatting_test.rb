38678d212d23096fc8626b6b38ac31f0532c022a
Giovanni Martins <giovannimartins2000@gmail.com>
2023-12-06 16:39:25 -0300

    solve linter report problems

--- test/requests/on_type_formatting_test.rb
+++ test/requests/on_type_formatting_test.rb
@@ -369,7 +369,7 @@ class OnTypeFormattingTest < Minitest::Test
 
     assert_equal(expected_edits.to_json, T.must(edits).to_json)
   end
-    
+
   def test_breaking_line_if_a_keyword_is_part_of_method_call
     document = RubyLsp::RubyDocument.new(source: +"  force({", version: 1, uri: URI("file:///fake.rb"))
     edits = RubyLsp::Requests::OnTypeFormatting.new(document, { line: 1, character: 2 }, "\n").run
