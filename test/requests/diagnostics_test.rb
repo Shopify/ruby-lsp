261a463b38362b45ddfdeb8fab4d236b550bcc45
Chris AtLee <chris.atlee@shopify.com>
2024-01-09 11:04:26 -0500

    Return empty diagnostics result if there are no syntax errors and rubocop isn't installed

--- test/requests/diagnostics_test.rb
+++ test/requests/diagnostics_test.rb
@@ -2,6 +2,7 @@
 # frozen_string_literal: true
 
 require "test_helper"
+require "ruby_lsp/requests/support/rubocop_diagnostics_runner"
 
 class DiagnosticsTest < Minitest::Test
   def test_empty_diagnostics_for_ignored_file
@@ -25,5 +26,36 @@ class DiagnosticsTest < Minitest::Test
 
     assert_equal(2, diagnostics.length)
     assert_equal("expected an `end` to close the `def` statement", T.must(diagnostics.last).message)
+  end
+
+  def test_empty_diagnostics_without_rubocop
+    document = RubyLsp::RubyDocument.new(source: <<~RUBY, version: 1, uri: URI("file:///fake/file.rb"))
+      def foo
+        "Hello, world!"
+      end
+    RUBY
+
+    klass = RubyLsp::Requests::Support::RuboCopDiagnosticsRunner
+    RubyLsp::Requests::Support.send(:remove_const, :RuboCopDiagnosticsRunner)
+
+    diagnostics = T.must(RubyLsp::Requests::Diagnostics.new(document).response)
+
+    assert_equal(0, diagnostics.length)
+  ensure
+    # Restore the class
+    RubyLsp::Requests::Support.const_set(:RuboCopDiagnosticsRunner, klass)
+  end
+
+  def test_empty_diagnostics_with_rubocop
+    document = RubyLsp::RubyDocument.new(source: <<~RUBY, version: 1, uri: URI("file:///fake/file.rb"))
+      def foo
+        "Hello, world!"
+      end
+    RUBY
+
+    # Make sure the rubocop runner is loaded
+    diagnostics = T.must(RubyLsp::Requests::Diagnostics.new(document).response)
+
+    assert_operator(diagnostics.length, :>, 0)
   end
 end
