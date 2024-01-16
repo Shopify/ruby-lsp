ceb99349474505edff8f78e37539426e7dd4c6b6
GregCornis <94356399+GregCornis@users.noreply.github.com>
2024-01-16 17:46:15 +0100

    Invoke shell command with separated arguments (#980)
    * Invoke shell command with separated arguments
    
    To support nushell, which doesn't accept having both arguments at the same time
    Fish, ZSH, Bash and sh are all compatible with this different syntax
    
    * Pass arguments to sheel in two times in `toolsExist`
    
    For Nushell compatibility

--- src/ruby.ts
+++ src/ruby.ts
@@ -174,7 +174,7 @@ export class Ruby implements RubyInterface {
   }
 
   private async activate(ruby: string) {
-    let command = this.shell ? `${this.shell} -ic '` : "";
+    let command = this.shell ? `${this.shell} -i -c '` : "";
 
     // The Ruby activation script is intentionally written as an array that gets joined into a one liner because some
     // terminals cannot handle line breaks. Do not switch this to a multiline string or that will break activation for
@@ -321,7 +321,7 @@ export class Ruby implements RubyInterface {
 
   private async toolExists(tool: string) {
     try {
-      let command = this.shell ? `${this.shell} -ic '` : "";
+      let command = this.shell ? `${this.shell} -i -c '` : "";
       command += `${tool} --version`;
 
       if (this.shell) {
