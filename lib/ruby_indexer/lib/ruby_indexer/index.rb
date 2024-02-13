8561d67d30ffec016c2a666f74e6d4f67b52b4f1
snutij <justin.rabiller@doctolib.com>
2024-02-13 21:53:58 +0100

    fix: resolve all methods not only the first one

--- lib/ruby_indexer/lib/ruby_indexer/index.rb
+++ lib/ruby_indexer/lib/ruby_indexer/index.rb
@@ -237,20 +237,20 @@ module RubyIndexer
       real_parts.join("::")
     end
 
-    # Attempts to find a given method for a resolved fully qualified receiver name. Returns `nil` if the method does not
-    # exist on that receiver
-    sig { params(method_name: String, receiver_name: String).returns(T.nilable(Entry::Member)) }
+    # Attempts to find methods for a resolved fully qualified receiver name.
+    # Returns an empty array if the method does not exist on that receiver
+    sig { params(method_name: String, receiver_name: String).returns(T::Array[Entry::Member]) }
     def resolve_method(method_name, receiver_name)
       method_entries = self[method_name]
       owner_entries = self[receiver_name]
-      return unless owner_entries && method_entries
+      return [] unless owner_entries && method_entries
 
       owner_name = T.must(owner_entries.first).name
       T.cast(
-        method_entries.grep(Entry::Member).find do |entry|
+        method_entries.grep(Entry::Member).select do |entry|
           T.cast(entry, Entry::Member).owner&.name == owner_name
         end,
-        T.nilable(Entry::Member),
+        T::Array[Entry::Member],
       )
     end
 
