5065de7cfc12a1ff667bf0fba476fdabb33ade7d
Andy Waite <andyw8@users.noreply.github.com>
2024-03-07 10:02:48 -0500

    Don't output warning during tests

--- lib/ruby_lsp/dependency_detector.rb
+++ lib/ruby_lsp/dependency_detector.rb
@@ -65,7 +65,8 @@ module RubyLsp
 
       Bundler.with_original_env do
         sorbet_static_detected = Bundler.locked_gems.specs.any? { |spec| spec.name == "sorbet-static" }
-        if sorbet_static_detected
+        # Don't show message while running tests, since it's noisy
+        if sorbet_static_detected && !defined?(Minitest)
           $stderr.puts("Ruby LSP detected this is a Sorbet project so will defer to it for some functionality")
         end
         sorbet_static_detected
