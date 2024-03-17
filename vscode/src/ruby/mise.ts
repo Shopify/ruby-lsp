73586dcb177e8372863810e6cc77886e45a7bc33
Jesse-Cameron <jessec.cameron@gmail.com>
2024-03-17 11:36:38 +1100

    feat: add support for homebrew mise

--- vscode/src/ruby/mise.ts
+++ vscode/src/ruby/mise.ts
@@ -12,20 +12,23 @@ import { VersionManager, ActivationResult } from "./versionManager";
 // Learn more: https://github.com/jdx/mise
 export class Mise extends VersionManager {
   async activate(): Promise<ActivationResult> {
-    const miseUri = vscode.Uri.joinPath(
-      vscode.Uri.file(os.homedir()),
-      ".local",
-      "bin",
-      "mise",
-    );
+    const possibleMiseInstallationUris = [
+      vscode.Uri.joinPath(
+        vscode.Uri.file(os.homedir()),
+        ".local",
+        "bin",
+        "mise",
+      ),
+      vscode.Uri.joinPath(
+        vscode.Uri.file("/"),
+        "opt",
+        "homebrew",
+        "bin",
+        "mise",
+      ),
+    ];
 
-    try {
-      vscode.workspace.fs.stat(miseUri);
-    } catch (error: any) {
-      throw new Error(
-        `The Ruby LSP version manager is configured to be Mise, but ${miseUri.fsPath} does not exist`,
-      );
-    }
+    const miseUri = await this.findMiseUri(possibleMiseInstallationUris);
 
     const activationScript =
       "STDERR.print({ env: ENV.to_h, yjit: !!defined?(RubyVM::YJIT), version: RUBY_VERSION }.to_json)";
@@ -44,5 +47,20 @@ export class Mise extends VersionManager {
       yjit: parsedResult.yjit,
       version: parsedResult.version,
     };
+  }
+
+  private async findMiseUri(params: vscode.Uri[]): Promise<vscode.Uri> {
+    for (const miseUri of params) {
+      try {
+        await vscode.workspace.fs.stat(miseUri);
+        return miseUri;
+      } catch (_error: any) {
+        // Continue to the next mise uri
+      }
+    }
+
+    throw new Error(
+      `The Ruby LSP version manager is configured to be Mise, but cannot find Mise in any of the expected paths`,
+    );
   }
 }
--- vscode/src/test/suite/ruby/mise.test.ts
+++ vscode/src/test/suite/ruby/mise.test.ts
@@ -16,7 +16,7 @@ suite("Mise", () => {
     return;
   }
 
-  test("Finds Ruby only binary path is appended to PATH", async () => {
+  test("Finds mise binary in default install location", async () => {
     // eslint-disable-next-line no-process-env
     const workspacePath = process.env.PWD!;
     const workspaceFolder = {
@@ -53,5 +53,56 @@ suite("Mise", () => {
     assert.deepStrictEqual(env.ANY, "true");
 
     execStub.restore();
+  });
+
+  test("Finds mise binary in homebrew install location", async () => {
+    if (os.platform() !== "darwin") {
+      // eslint-disable-next-line no-console
+      console.log("Skipping Mise test on non-windows");
+      return;
+    }
+    // eslint-disable-next-line no-process-env
+    const workspacePath = process.env.PWD!;
+    const workspaceFolder = {
+      uri: vscode.Uri.from({ scheme: "file", path: workspacePath }),
+      name: path.basename(workspacePath),
+      index: 0,
+    };
+    const outputChannel = new WorkspaceChannel("fake", common.LOG_CHANNEL);
+    const mise = new Mise(workspaceFolder, outputChannel);
+
+    const activationScript =
+      "STDERR.print({ env: ENV.to_h, yjit: !!defined?(RubyVM::YJIT), version: RUBY_VERSION }.to_json)";
+
+    const execStub = sinon.stub(common, "asyncExec").resolves({
+      stdout: "",
+      stderr: JSON.stringify({
+        env: { ANY: "true" },
+        yjit: true,
+        version: "3.0.0",
+      }),
+    });
+
+    // pretend that the default mise install location doesn't exist
+    const statStub = sinon
+      .stub(vscode.workspace.fs, "stat")
+      .rejects()
+      .onFirstCall();
+
+    const { env, version, yjit } = await mise.activate();
+
+    assert.ok(
+      execStub.calledOnceWithExactly(
+        `/opt/homebrew/bin/mise x -- ruby -W0 -rjson -e '${activationScript}'`,
+        { cwd: workspacePath },
+      ),
+    );
+
+    assert.strictEqual(version, "3.0.0");
+    assert.strictEqual(yjit, true);
+    assert.deepStrictEqual(env.ANY, "true");
+
+    execStub.restore();
+    statStub.restore();
   });
 });
