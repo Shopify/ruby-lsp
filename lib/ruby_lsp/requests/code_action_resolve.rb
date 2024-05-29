c2c17c7e227298e802409301afa45b56e3093918
Andy Waite <13400+andyw8@users.noreply.github.com>
2024-05-29 12:50:12 -0400

    Fix types

--- lib/ruby_lsp/requests/code_action_resolve.rb
+++ lib/ruby_lsp/requests/code_action_resolve.rb
@@ -166,7 +166,7 @@ module RubyLsp
         # Find the closest method declaration node, so that we place the refactor in a valid position
         # closest_def, _ = T.cast(
         target_context = @document.locate(@document.tree, start_index, node_types: [Prism::DefNode])
-        closest_def = target_context.closest
+        closest_def = T.cast(target_context.closest, Prism::DefNode)
         return Error::InvalidTargetRange if closest_def.nil?
 
         end_keyword_loc = closest_def.end_keyword_loc
