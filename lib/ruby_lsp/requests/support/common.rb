4e95b749494b94923f3afe1b96ae37794f928b61
Alexandre Terrasa <alexandre.terrasa@shopify.com>
2024-06-04 11:34:34 -0400

    Move `kind_for_entry` helper to Support::Common
    Signed-off-by: Alexandre Terrasa <alexandre.terrasa@shopify.com>

--- lib/ruby_lsp/requests/support/common.rb
+++ lib/ruby_lsp/requests/support/common.rb
@@ -186,6 +186,24 @@ module RubyLsp
             current = current.parent
           end
         end
+
+        sig { params(entry: RubyIndexer::Entry).returns(T.nilable(Integer)) }
+        def kind_for_entry(entry)
+          case entry
+          when RubyIndexer::Entry::Class
+            Constant::SymbolKind::CLASS
+          when RubyIndexer::Entry::Module
+            Constant::SymbolKind::NAMESPACE
+          when RubyIndexer::Entry::Constant
+            Constant::SymbolKind::CONSTANT
+          when RubyIndexer::Entry::Method
+            entry.name == "initialize" ? Constant::SymbolKind::CONSTRUCTOR : Constant::SymbolKind::METHOD
+          when RubyIndexer::Entry::Accessor
+            Constant::SymbolKind::PROPERTY
+          when RubyIndexer::Entry::InstanceVariable
+            Constant::SymbolKind::FIELD
+          end
+        end
       end
     end
   end
