66d35bd754643cf44d53dd405539ff4730a0c230
Andy Waite <13400+andyw8@users.noreply.github.com>
2024-08-28 10:11:43 -0400

    Add entries_for

--- lib/ruby_indexer/lib/ruby_indexer/index.rb
+++ lib/ruby_indexer/lib/ruby_indexer/index.rb
@@ -616,6 +616,18 @@ module RubyIndexer
       singleton
     end
 
+    sig do
+      type_parameters(:T).params(
+        path: String,
+        type: T.nilable(T::Class[T.all(T.type_parameter(:T), Entry)]),
+      ).returns(T.nilable(T::Array[T.type_parameter(:T)]))
+    end
+    def entries_for(path, type = nil)
+      type = Entry if type.nil?
+
+      @files_to_entries[path]&.grep(type)&.grep_v(RubyIndexer::Entry::SingletonClass)
+    end
+
     private
 
     # Runs the registered included hooks
