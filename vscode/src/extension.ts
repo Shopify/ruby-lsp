dd57908707f452f5d523cbffddeeb3a66d6956fc
Vinicius Stock <vinistock@users.noreply.github.com>
2024-09-23 15:31:39 -0400

    Remove version manager setting migration code (#2600)

--- vscode/src/extension.ts
+++ vscode/src/extension.ts
@@ -8,8 +8,6 @@ import { LOG_CHANNEL } from "./common";
 let extension: RubyLsp;
 
 export async function activate(context: vscode.ExtensionContext) {
-  await migrateManagerConfigurations();
-
   if (!vscode.workspace.workspaceFolders) {
     // We currently don't support usage without any workspace folders opened. Here we warn the user, point to the issue
     // and offer to open a folder instead
@@ -36,38 +34,6 @@ export async function activate(context: vscode.ExtensionContext) {
 
 export async function deactivate(): Promise<void> {
   await extension.deactivate();
-}
-
-type InspectKeys =
-  | "globalValue"
-  | "workspaceValue"
-  | "workspaceFolderValue"
-  | "globalLanguageValue"
-  | "workspaceLanguageValue"
-  | "workspaceFolderLanguageValue";
-// Function to migrate the old version manager configuration to the new format. Remove this after a few months
-async function migrateManagerConfigurations() {
-  const configuration = vscode.workspace.getConfiguration("rubyLsp");
-  const currentManagerSettings =
-    configuration.inspect<string>("rubyVersionManager")!;
-  let identifier: string | undefined;
-
-  const targetMap: Record<InspectKeys, vscode.ConfigurationTarget> = {
-    globalValue: vscode.ConfigurationTarget.Global,
-    globalLanguageValue: vscode.ConfigurationTarget.Global,
-    workspaceFolderLanguageValue: vscode.ConfigurationTarget.WorkspaceFolder,
-    workspaceFolderValue: vscode.ConfigurationTarget.WorkspaceFolder,
-    workspaceLanguageValue: vscode.ConfigurationTarget.Workspace,
-    workspaceValue: vscode.ConfigurationTarget.Workspace,
-  };
-
-  for (const [key, target] of Object.entries(targetMap)) {
-    identifier = currentManagerSettings[key as InspectKeys];
-
-    if (identifier && typeof identifier === "string") {
-      await configuration.update("rubyVersionManager", { identifier }, target);
-    }
-  }
 }
 
 async function createLogger(context: vscode.ExtensionContext) {
