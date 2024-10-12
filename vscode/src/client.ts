0b309b7fbd4c0e14a49af8dec316f58326395e95
Vinicius Stock <vinicius.stock@shopify.com>
2024-10-12 10:39:40 -0400

    Use activated gem paths to register document selectors

--- vscode/src/client.ts
+++ vscode/src/client.ts
@@ -126,6 +126,11 @@ function collectClientOptions(
   const enabledFeatures = Object.keys(features).filter((key) => features[key]);
 
   const fsPath = workspaceFolder.uri.fsPath.replace(/\/$/, "");
+
+  // For each workspace, the language client is responsible for handling requests for:
+  // 1. Files inside of the workspace itself
+  // 2. Bundled gems
+  // 3. Default gems
   let documentSelector: DocumentSelector = SUPPORTED_LANGUAGE_IDS.map(
     (language) => {
       return { language, pattern: `${fsPath}/**/*` };
@@ -143,27 +148,18 @@ function collectClientOptions(
     });
   }
 
-  // For each workspace, the language client is responsible for handling requests for:
-  // 1. Files inside of the workspace itself
-  // 2. Bundled gems
-  // 3. Default gems
-
-  if (ruby.env.GEM_PATH) {
-    const parts = ruby.env.GEM_PATH.split(path.delimiter);
-
-    // Because of how default gems are installed, the entry in the `GEM_PATH` is actually not exactly where the files
-    // are located. With the regex, we are correcting the default gem path from this (where the files are not located)
-    // /opt/rubies/3.3.1/lib/ruby/gems/3.3.0
-    //
-    // to this (where the files are actually stored)
-    // /opt/rubies/3.3.1/lib/ruby/3.3.0
-    parts.forEach((gemPath) => {
-      documentSelector.push({
-        language: "ruby",
-        pattern: `${gemPath.replace(/lib\/ruby\/gems\/(?=\d)/, "lib/ruby/")}/**/*`,
-      });
+  // Because of how default gems are installed, the entry in the `GEM_PATH` is actually not exactly where the files
+  // are located. With the regex, we are correcting the default gem path from this (where the files are not located)
+  // /opt/rubies/3.3.1/lib/ruby/gems/3.3.0
+  //
+  // to this (where the files are actually stored)
+  // /opt/rubies/3.3.1/lib/ruby/3.3.0
+  ruby.gemPath.forEach((gemPath) => {
+    documentSelector.push({
+      language: "ruby",
+      pattern: `${gemPath.replace(/lib\/ruby\/gems\/(?=\d)/, "lib/ruby/")}/**/*`,
     });
-  }
+  });
 
   // This is a temporary solution as an escape hatch for users who cannot upgrade the `ruby-lsp` gem to a version that
   // supports ERB
