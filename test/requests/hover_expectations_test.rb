75852a14a45eab9eddd3750327b55d38d96f0f00
snutij <justin.rabiller@doctolib.com>
2024-10-14 09:21:19 +0200

    feat: apply hover target correction on global variable

--- test/requests/hover_expectations_test.rb
+++ test/requests/hover_expectations_test.rb
@@ -102,6 +102,36 @@ class HoverExpectationsTest < ExpectationsTestRunner
     end
   end
 
+  def test_hover_apply_target_correction
+    source = <<~RUBY
+      # and write node
+      $bar &&= 1
+      # operator write node
+      $baz += 1
+      # or write node
+      $qux ||= 1
+      # write node
+      $foo = 1
+    RUBY
+
+    source_line_nodes = [1, 3, 5, 7]
+
+    with_server(source) do |server, uri|
+      source_line_nodes.each do |line|
+        server.process_message(
+          id: 1,
+          method: "textDocument/hover",
+          params: {
+            textDocument: { uri: uri },
+            position: { line: line, character: 5 },
+          },
+        )
+
+        assert_nil(server.pop_response.response)
+      end
+    end
+  end
+
   def test_hovering_precision
     source = <<~RUBY
       module Foo
@@ -352,13 +382,13 @@ class HoverExpectationsTest < ExpectationsTestRunner
       end
     RUBY
 
-    # Going to definition on `argument` should not take you to the `foo` method definition
     with_server(source) do |server, uri|
       server.process_message(
         id: 1,
         method: "textDocument/hover",
         params: { textDocument: { uri: uri }, position: { character: 12, line: 5 } },
       )
+      # Hover on `argument` should not show you the `foo` documentation
       assert_nil(server.pop_response.response)
 
       server.process_message(
