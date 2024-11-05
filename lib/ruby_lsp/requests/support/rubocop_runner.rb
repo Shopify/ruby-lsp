429bb3317c8ce68bee0a74bb8a1cc9efb86a01f5
vinibispo <vini.bispo015@gmail.com>
2024-11-05 14:04:28 -0300

    Add reminder to update the documentation when changing the minimum version of Rubocop

--- lib/ruby_lsp/requests/support/rubocop_runner.rb
+++ lib/ruby_lsp/requests/support/rubocop_runner.rb
@@ -15,6 +15,7 @@ rescue LoadError
   return
 end
 
+# Remember to update the version in the documentation (usage/implicit-dependencies section) if you change this
 # Ensure that RuboCop is at least version 1.4.0
 begin
   gem("rubocop", ">= 1.4.0")
