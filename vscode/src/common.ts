e5e7d9767cd1adb55ce20e7f430228d549b05b32
Vinicius Stock <vinistock@users.noreply.github.com>
2024-11-27 09:11:26 -0500

    Rollout launcher mode to 5% of users (#2914)
    ### Motivation
    
    We've been using the new launcher mode for the server for a few weeks now, with zero issues encountered so far.
    
    Let's roll it out to 5% of users. A larger group of users will help surface any other errors in our telemetry.
    
    ### Implementation
    
    Instead of having a setting dedicated to the launcher, I just made it a feature flag, which is what we should do going forward.

--- vscode/src/common.ts
+++ vscode/src/common.ts
@@ -80,6 +80,7 @@ export const SUPPORTED_LANGUAGE_IDS = ["ruby", "erb"];
 // Note: names added here should also be added to the `rubyLsp.optedOutFeatureFlags` enum in the `package.json` file
 export const FEATURE_FLAGS = {
   tapiocaAddon: 0.0,
+  launcher: 0.05,
 };
 
 type FeatureFlagConfigurationKey = keyof typeof FEATURE_FLAGS | "all";
