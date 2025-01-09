Shopify/ruby-lsp

commit 413eaea619438147e0e9c1db2bc984b263d813a3

Locate targets under mutex lock (#2976)
### Motivation

I'm hoping this is the solution for #2446

My hypothesis is that we're seeing a concurrency issue where threads switch in the middle of locating targets and then the document gets mutated. That would render the location we're currently searching for invalid, which can then lead to infinite loops.

### Implementation

My idea is to move our mutex into the global state so that we can use it in more places. Then we lock the mutex while locating targets to avoid having any document edits be applied in the middle.

**Note**: I experimented with passing the mutex down to the document instances, but it was a bit messier.

lib/ruby_lsp/document.rb
@@ -43,13 +43,14 @@ module RubyLsp
     sig { returns(T.any(Interface::SemanticTokens, Object)) }
     attr_accessor :semantic_tokens
 
-    sig { params(source: String, version: Integer, uri: URI::Generic, encoding: Encoding).void }
-    def initialize(source:, version:, uri:, encoding: Encoding::UTF_8)
+    sig { params(source: String, version: Integer, uri: URI::Generic, global_state: GlobalState).void }
+    def initialize(source:, version:, uri:, global_state:)
+      @source = source
+      @version = version
+      @global_state = global_state
       @cache = T.let(Hash.new(EMPTY_CACHE), T::Hash[String, T.untyped])
       @semantic_tokens = T.let(EMPTY_CACHE, T.any(Interface::SemanticTokens, Object))
-      @encoding = T.let(encoding, Encoding)
-      @source = T.let(source, String)
-      @version = T.let(version, Integer)
+      @encoding = T.let(global_state.encoding, Encoding)
       @uri = T.let(uri, URI::Generic)
       @needs_parsing = T.let(true, T::Boolean)
       @parse_result = T.let(T.unsafe(nil), ParseResultType)
@@ -64,7 +65,6 @@ module RubyLsp
     sig { abstract.returns(LanguageId) }
     def language_id; end
 
-    # TODO: remove this method once all non-positional requests have been migrated to the listener pattern
     sig do
       type_parameters(:T)
         .params(
@@ -93,19 +93,21 @@ module RubyLsp
 
     sig { params(edits: T::Array[T::Hash[Symbol, T.untyped]], version: Integer).void }
     def push_edits(edits, version:)
-      edits.each do |edit|
-        range = edit[:range]
-        scanner = create_scanner
+      @global_state.synchronize do
+        edits.each do |edit|
+          range = edit[:range]
+          scanner = create_scanner
 
-        start_position = scanner.find_char_position(range[:start])
-        end_position = scanner.find_char_position(range[:end])
+          start_position = scanner.find_char_position(range[:start])
+          end_position = scanner.find_char_position(range[:end])
 
-        @source[start_position...end_position] = edit[:text]
+          @source[start_position...end_position] = edit[:text]
+        end
+
+        @version = version
+        @needs_parsing = true
+        @cache.clear
       end
-
-      @version = version
-      @needs_parsing = true
-      @cache.clear
     end
 
     # Returns `true` if the document was parsed and `false` if nothing needed parsing
@@ -115,14 +117,31 @@ module RubyLsp
     sig { abstract.returns(T::Boolean) }
     def syntax_error?; end
 
-    sig { returns(Scanner) }
-    def create_scanner
-      Scanner.new(@source, @encoding)
-    end
-
     sig { returns(T::Boolean) }
     def past_expensive_limit?
       @source.length > MAXIMUM_CHARACTERS_FOR_EXPENSIVE_FEATURES
+    end
+
+    sig do
+      params(
+        start_pos: T::Hash[Symbol, T.untyped],
+        end_pos: T.nilable(T::Hash[Symbol, T.untyped]),
+      ).returns([Integer, T.nilable(Integer)])
+    end
+    def find_index_by_position(start_pos, end_pos = nil)
+      @global_state.synchronize do
+        scanner = create_scanner
+        start_index = scanner.find_char_position(start_pos)
+        end_index = scanner.find_char_position(end_pos) if end_pos
+        [start_index, end_index]
+      end
+    end
+
+    private
+
+    sig { returns(Scanner) }
+    def create_scanner
+      Scanner.new(@source, @encoding)
     end
 
     class Scanner

lib/ruby_lsp/server.rb
@@ -107,7 +107,7 @@ module RubyLsp
           ),
         )
       when "$/cancelRequest"
-        @mutex.synchronize { @cancelled_requests << message[:params][:id] }
+        @global_state.synchronize { @cancelled_requests << message[:params][:id] }
       when nil
         process_response(message) if message[:result]
       end
@@ -377,58 +377,53 @@ module RubyLsp
 
     sig { params(message: T::Hash[Symbol, T.untyped]).void }
     def text_document_did_open(message)
-      @mutex.synchronize do
-        text_document = message.dig(:params, :textDocument)
-        language_id = case text_document[:languageId]
-        when "erb", "eruby"
-          Document::LanguageId::ERB
-        when "rbs"
-          Document::LanguageId::RBS
-        else
-          Document::LanguageId::Ruby
-        end
+      text_document = message.dig(:params, :textDocument)
+      language_id = case text_document[:languageId]
+      when "erb", "eruby"
+        Document::LanguageId::ERB
+      when "rbs"
+        Document::LanguageId::RBS
+      else
+        Document::LanguageId::Ruby
+      end
 
-        document = @store.set(
-          uri: text_document[:uri],
-          source: text_document[:text],
-          version: text_document[:version],
-          encoding: @global_state.encoding,
-          language_id: language_id,
-        )
+      document = @store.set(
+        uri: text_document[:uri],
+        source: text_document[:text],
+        version: text_document[:version],
+        language_id: language_id,
+      )
 
-        if document.past_expensive_limit? && text_document[:uri].scheme == "file"
-          log_message = <<~MESSAGE
-            The file #{text_document[:uri].path} is too long. For performance reasons, semantic highlighting and
-            diagnostics will be disabled.
-          MESSAGE
+      if document.past_expensive_limit? && text_document[:uri].scheme == "file"
+        log_message = <<~MESSAGE
+          The file #{text_document[:uri].path} is too long. For performance reasons, semantic highlighting and
+          diagnostics will be disabled.
+        MESSAGE
 
-          send_message(
-            Notification.new(
-              method: "window/logMessage",
-              params: Interface::LogMessageParams.new(
-                type: Constant::MessageType::WARNING,
-                message: log_message,
-              ),
+        send_message(
+          Notification.new(
+            method: "window/logMessage",
+            params: Interface::LogMessageParams.new(
+              type: Constant::MessageType::WARNING,
+              message: log_message,
             ),
-          )
-        end
+          ),
+        )
       end
     end
 
     sig { params(message: T::Hash[Symbol, T.untyped]).void }
     def text_document_did_close(message)
-      @mutex.synchronize do
-        uri = message.dig(:params, :textDocument, :uri)
-        @store.delete(uri)
+      uri = message.dig(:params, :textDocument, :uri)
+      @store.delete(uri)
 
-        # Clear diagnostics for the closed file, so that they no longer appear in the problems tab
-        send_message(
-          Notification.new(
-            method: "textDocument/publishDiagnostics",
-            params: Interface::PublishDiagnosticsParams.new(uri: uri.to_s, diagnostics: []),
-          ),
-        )
-      end
+      # Clear diagnostics for the closed file, so that they no longer appear in the problems tab
+      send_message(
+        Notification.new(
+          method: "textDocument/publishDiagnostics",
+          params: Interface::PublishDiagnosticsParams.new(uri: uri.to_s, diagnostics: []),
+        ),
+      )
     end
 
     sig { params(message: T::Hash[Symbol, T.untyped]).void }
@@ -436,9 +431,7 @@ module RubyLsp
       params = message[:params]
       text_document = params[:textDocument]
 
-      @mutex.synchronize do
-        @store.push_edits(uri: text_document[:uri], edits: params[:contentChanges], version: text_document[:version])
-      end
+      @store.push_edits(uri: text_document[:uri], edits: params[:contentChanges], version: text_document[:version])
     end
 
     sig { params(message: T::Hash[Symbol, T.untyped]).void }

lib/ruby_lsp/store.rb
@@ -13,8 +13,9 @@ module RubyLsp
     sig { returns(String) }
     attr_accessor :client_name
 
-    sig { void }
-    def initialize
+    sig { params(global_state: GlobalState).void }
+    def initialize(global_state)
+      @global_state = global_state
       @state = T.let({}, T::Hash[String, Document[T.untyped]])
       @features_configuration = T.let(
         {
@@ -61,17 +62,18 @@ module RubyLsp
         source: String,
         version: Integer,
         language_id: Document::LanguageId,
-        encoding: Encoding,
       ).returns(Document[T.untyped])
     end
-    def set(uri:, source:, version:, language_id:, encoding: Encoding::UTF_8)
-      @state[uri.to_s] = case language_id
-      when Document::LanguageId::ERB
-        ERBDocument.new(source: source, version: version, uri: uri, encoding: encoding)
-      when Document::LanguageId::RBS
-        RBSDocument.new(source: source, version: version, uri: uri, encoding: encoding)
-      else
-        RubyDocument.new(source: source, version: version, uri: uri, encoding: encoding)
+    def set(uri:, source:, version:, language_id:)
+      @global_state.synchronize do
+        @state[uri.to_s] = case language_id
+        when Document::LanguageId::ERB
+          ERBDocument.new(source: source, version: version, uri: uri, global_state: @global_state)
+        when Document::LanguageId::RBS
+          RBSDocument.new(source: source, version: version, uri: uri, global_state: @global_state)
+        else
+          RubyDocument.new(source: source, version: version, uri: uri, global_state: @global_state)
+        end
       end
     end
 
@@ -92,7 +94,9 @@ module RubyLsp
 
     sig { params(uri: URI::Generic).void }
     def delete(uri)
-      @state.delete(uri.to_s)
+      @global_state.synchronize do
+        @state.delete(uri.to_s)
+      end
     end
 
     sig { params(uri: URI::Generic).returns(T::Boolean) }

lib/ruby_lsp/base_server.rb
@@ -18,22 +18,21 @@ module RubyLsp
       @incoming_queue = T.let(Thread::Queue.new, Thread::Queue)
       @outgoing_queue = T.let(Thread::Queue.new, Thread::Queue)
       @cancelled_requests = T.let([], T::Array[Integer])
-      @mutex = T.let(Mutex.new, Mutex)
       @worker = T.let(new_worker, Thread)
       @current_request_id = T.let(1, Integer)
-      @store = T.let(Store.new, Store)
+      @global_state = T.let(GlobalState.new, GlobalState)
+      @store = T.let(Store.new(@global_state), Store)
       @outgoing_dispatcher = T.let(
         Thread.new do
           unless @test_mode
             while (message = @outgoing_queue.pop)
-              @mutex.synchronize { @writer.write(message.to_hash) }
+              @global_state.synchronize { @writer.write(message.to_hash) }
             end
           end
         end,
         Thread,
       )
 
-      @global_state = T.let(GlobalState.new, GlobalState)
       Thread.main.priority = 1
 
       # We read the initialize request in `exe/ruby-lsp` to be able to determine the workspace URI where Bundler should
@@ -51,7 +50,7 @@ module RubyLsp
         # source. Altering the source reference during parsing will put the parser in an invalid internal state, since
         # it started parsing with one source but then it changed in the middle. We don't want to do this for text
         # synchronization notifications
-        @mutex.synchronize do
+        @global_state.synchronize do
           uri = message.dig(:params, :textDocument, :uri)
 
           if uri
@@ -95,14 +94,14 @@ module RubyLsp
              "$/cancelRequest"
           process_message(message)
         when "shutdown"
-          @mutex.synchronize do
+          @global_state.synchronize do
             send_log_message("Shutting down Ruby LSP...")
             shutdown
             run_shutdown
             @writer.write(Result.new(id: message[:id], response: nil).to_hash)
           end
         when "exit"
-          @mutex.synchronize do
+          @global_state.synchronize do
             status = @incoming_queue.closed? ? 0 : 1
             send_log_message("Shutdown complete with status #{status}")
             exit(status)
@@ -157,7 +156,7 @@ module RubyLsp
           id = message[:id]
 
           # Check if the request was cancelled before trying to process it
-          @mutex.synchronize do
+          @global_state.synchronize do
             if id && @cancelled_requests.include?(id)
               send_message(Error.new(
                 id: id,

lib/ruby_lsp/erb_document.rb
@@ -19,8 +19,8 @@ module RubyLsp
     end
     attr_reader :code_units_cache
 
-    sig { params(source: String, version: Integer, uri: URI::Generic, encoding: Encoding).void }
-    def initialize(source:, version:, uri:, encoding: Encoding::UTF_8)
+    sig { params(source: String, version: Integer, uri: URI::Generic, global_state: GlobalState).void }
+    def initialize(source:, version:, uri:, global_state:)
       # This has to be initialized before calling super because we call `parse` in the parent constructor, which
       # overrides this with the proper virtual host language source
       @host_language_source = T.let("", String)
@@ -63,9 +63,11 @@ module RubyLsp
       ).returns(NodeContext)
     end
     def locate_node(position, node_types: [])
+      char_position, _ = find_index_by_position(position)
+
       RubyDocument.locate(
         @parse_result.value,
-        create_scanner.find_char_position(position),
+        char_position,
         code_units_cache: @code_units_cache,
         node_types: node_types,
       )

lib/ruby_lsp/global_state.rb
@@ -53,6 +53,12 @@ module RubyLsp
       )
       @client_capabilities = T.let(ClientCapabilities.new, ClientCapabilities)
       @enabled_feature_flags = T.let({}, T::Hash[Symbol, T::Boolean])
+      @mutex = T.let(Mutex.new, Mutex)
+    end
+
+    sig { type_parameters(:T).params(block: T.proc.returns(T.type_parameter(:T))).returns(T.type_parameter(:T)) }
+    def synchronize(&block)
+      @mutex.synchronize(&block)
     end
 
     sig { params(addon_name: String).returns(T.nilable(T::Hash[Symbol, T.untyped])) }

lib/ruby_lsp/rbs_document.rb
@@ -8,8 +8,8 @@ module RubyLsp
 
     ParseResultType = type_member { { fixed: T::Array[RBS::AST::Declarations::Base] } }
 
-    sig { params(source: String, version: Integer, uri: URI::Generic, encoding: Encoding).void }
-    def initialize(source:, version:, uri:, encoding: Encoding::UTF_8)
+    sig { params(source: String, version: Integer, uri: URI::Generic, global_state: GlobalState).void }
+    def initialize(source:, version:, uri:, global_state:)
       @syntax_error = T.let(false, T::Boolean)
       super
     end

lib/ruby_lsp/requests/code_action_resolve.rb
@@ -92,9 +92,7 @@ module RubyLsp
         source_range = @code_action.dig(:data, :range)
         return Error::EmptySelection if source_range[:start] == source_range[:end]
 
-        scanner = @document.create_scanner
-        start_index = scanner.find_char_position(source_range[:start])
-        end_index = scanner.find_char_position(source_range[:end])
+        start_index, end_index = @document.find_index_by_position(source_range[:start], source_range[:end])
         extracted_source = T.must(@document.source[start_index...end_index])
 
         # Find the closest statements node, so that we place the refactor in a valid position
@@ -192,9 +190,7 @@ module RubyLsp
         source_range = @code_action.dig(:data, :range)
         return Error::EmptySelection if source_range[:start] == source_range[:end]
 
-        scanner = @document.create_scanner
-        start_index = scanner.find_char_position(source_range[:start])
-        end_index = scanner.find_char_position(source_range[:end])
+        start_index, end_index = @document.find_index_by_position(source_range[:start], source_range[:end])
         extracted_source = T.must(@document.source[start_index...end_index])
 
         # Find the closest method declaration node, so that we place the refactor in a valid position

commit af45f856bf7444aedf37ce4de4df362d1e20d750

Synchronize store operations in server instead (#3029)
### Motivation

Closes #3027

The deadlock bug was introduced in #2976. Essentially, if the editor opens a URI for a non-existing file, we try to acquire the same lock twice.

Once [here](https://github.com/Shopify/ruby-lsp/blob/17d5fc733b758bbbedaf36fb17ac3914548086bf/lib/ruby_lsp/base_server.rb#L53) to verify if the request requires a document to be parsed and the second time [here](https://github.com/Shopify/ruby-lsp/blob/17d5fc733b758bbbedaf36fb17ac3914548086bf/lib/ruby_lsp/store.rb#L68) when we read the file from disk and save it in the store.

Trying to acquire the same mutex lock twice raises the deadlock error.

### Implementation

I stopped synchronizing anything inside basic store operations. It just increases the chance of mistakes like these. I moved the responsibility of synchronizing back to the server.

### Automated Tests

To catch this regression, we need a client/server integration test because the first synchronization happens only by reading a request from the STDIN pipe. I added one to ensure we don't hit the same issue again.

lib/ruby_lsp/document.rb
@@ -97,33 +97,31 @@ module RubyLsp
 
     sig { params(edits: T::Array[T::Hash[Symbol, T.untyped]], version: Integer).void }
     def push_edits(edits, version:)
-      @global_state.synchronize do
-        edits.each do |edit|
-          range = edit[:range]
-          scanner = create_scanner
+      edits.each do |edit|
+        range = edit[:range]
+        scanner = create_scanner
 
-          start_position = scanner.find_char_position(range[:start])
-          end_position = scanner.find_char_position(range[:end])
+        start_position = scanner.find_char_position(range[:start])
+        end_position = scanner.find_char_position(range[:end])
 
-          @source[start_position...end_position] = edit[:text]
-        end
+        @source[start_position...end_position] = edit[:text]
+      end
 
-        @version = version
-        @needs_parsing = true
-        @cache.clear
+      @version = version
+      @needs_parsing = true
+      @cache.clear
 
-        last_edit = edits.last
-        return unless last_edit
+      last_edit = edits.last
+      return unless last_edit
 
-        last_edit_range = last_edit[:range]
+      last_edit_range = last_edit[:range]
 
-        @last_edit = if last_edit_range[:start] == last_edit_range[:end]
-          Insert.new(last_edit_range)
-        elsif last_edit[:text].empty?
-          Delete.new(last_edit_range)
-        else
-          Replace.new(last_edit_range)
-        end
+      @last_edit = if last_edit_range[:start] == last_edit_range[:end]
+        Insert.new(last_edit_range)
+      elsif last_edit[:text].empty?
+        Delete.new(last_edit_range)
+      else
+        Replace.new(last_edit_range)
       end
     end
 

lib/ruby_lsp/server.rb
@@ -359,48 +359,52 @@ module RubyLsp
 
     sig { params(message: T::Hash[Symbol, T.untyped]).void }
     def text_document_did_open(message)
-      text_document = message.dig(:params, :textDocument)
-      language_id = case text_document[:languageId]
-      when "erb", "eruby"
-        Document::LanguageId::ERB
-      when "rbs"
-        Document::LanguageId::RBS
-      else
-        Document::LanguageId::Ruby
-      end
+      @global_state.synchronize do
+        text_document = message.dig(:params, :textDocument)
+        language_id = case text_document[:languageId]
+        when "erb", "eruby"
+          Document::LanguageId::ERB
+        when "rbs"
+          Document::LanguageId::RBS
+        else
+          Document::LanguageId::Ruby
+        end
 
-      document = @store.set(
-        uri: text_document[:uri],
-        source: text_document[:text],
-        version: text_document[:version],
-        language_id: language_id,
-      )
-
-      if document.past_expensive_limit? && text_document[:uri].scheme == "file"
-        log_message = <<~MESSAGE
-          The file #{text_document[:uri].path} is too long. For performance reasons, semantic highlighting and
-          diagnostics will be disabled.
-        MESSAGE
-
-        send_message(
-          Notification.new(
-            method: "window/logMessage",
-            params: Interface::LogMessageParams.new(
-              type: Constant::MessageType::WARNING,
-              message: log_message,
-            ),
-          ),
+        document = @store.set(
+          uri: text_document[:uri],
+          source: text_document[:text],
+          version: text_document[:version],
+          language_id: language_id,
         )
+
+        if document.past_expensive_limit? && text_document[:uri].scheme == "file"
+          log_message = <<~MESSAGE
+            The file #{text_document[:uri].path} is too long. For performance reasons, semantic highlighting and
+            diagnostics will be disabled.
+          MESSAGE
+
+          send_message(
+            Notification.new(
+              method: "window/logMessage",
+              params: Interface::LogMessageParams.new(
+                type: Constant::MessageType::WARNING,
+                message: log_message,
+              ),
+            ),
+          )
+        end
       end
     end
 
     sig { params(message: T::Hash[Symbol, T.untyped]).void }
     def text_document_did_close(message)
-      uri = message.dig(:params, :textDocument, :uri)
-      @store.delete(uri)
+      @global_state.synchronize do
+        uri = message.dig(:params, :textDocument, :uri)
+        @store.delete(uri)
 
-      # Clear diagnostics for the closed file, so that they no longer appear in the problems tab
-      send_message(Notification.publish_diagnostics(uri.to_s, []))
+        # Clear diagnostics for the closed file, so that they no longer appear in the problems tab
+        send_message(Notification.publish_diagnostics(uri.to_s, []))
+      end
     end
 
     sig { params(message: T::Hash[Symbol, T.untyped]).void }
@@ -408,7 +412,9 @@ module RubyLsp
       params = message[:params]
       text_document = params[:textDocument]
 
-      @store.push_edits(uri: text_document[:uri], edits: params[:contentChanges], version: text_document[:version])
+      @global_state.synchronize do
+        @store.push_edits(uri: text_document[:uri], edits: params[:contentChanges], version: text_document[:version])
+      end
     end
 
     sig { params(message: T::Hash[Symbol, T.untyped]).void }

lib/ruby_lsp/store.rb
@@ -65,15 +65,13 @@ module RubyLsp
       ).returns(Document[T.untyped])
     end
     def set(uri:, source:, version:, language_id:)
-      @global_state.synchronize do
-        @state[uri.to_s] = case language_id
-        when Document::LanguageId::ERB
-          ERBDocument.new(source: source, version: version, uri: uri, global_state: @global_state)
-        when Document::LanguageId::RBS
-          RBSDocument.new(source: source, version: version, uri: uri, global_state: @global_state)
-        else
-          RubyDocument.new(source: source, version: version, uri: uri, global_state: @global_state)
-        end
+      @state[uri.to_s] = case language_id
+      when Document::LanguageId::ERB
+        ERBDocument.new(source: source, version: version, uri: uri, global_state: @global_state)
+      when Document::LanguageId::RBS
+        RBSDocument.new(source: source, version: version, uri: uri, global_state: @global_state)
+      else
+        RubyDocument.new(source: source, version: version, uri: uri, global_state: @global_state)
       end
     end
 
@@ -94,9 +92,7 @@ module RubyLsp
 
     sig { params(uri: URI::Generic).void }
     def delete(uri)
-      @global_state.synchronize do
-        @state.delete(uri.to_s)
-      end
+      @state.delete(uri.to_s)
     end
 
     sig { params(uri: URI::Generic).returns(T::Boolean) }

vscode/src/test/suite/client.test.ts
@@ -1034,4 +1034,24 @@ suite("Client", () => {
       ),
     );
   }).timeout(20000);
+
+  test("requests for documents that were not opened by the client", async () => {
+    const uri = vscode.Uri.joinPath(
+      workspaceUri,
+      "lib",
+      "ruby_lsp",
+      "server.rb",
+    );
+
+    const response: vscode.DocumentSymbol[] = await client.sendRequest(
+      "textDocument/documentSymbol",
+      {
+        textDocument: {
+          uri: uri.toString(),
+        },
+      },
+    );
+
+    assert.ok(response.length > 0);
+  }).timeout(20000);
 });