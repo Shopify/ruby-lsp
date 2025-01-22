e0acffc65fdfddadfabe495549fc7f17658a525e
Jenny Shih <jinghua.shih@gmail.com>
2025-01-22 14:59:32 -0500

    Add experimental/gotoRelevantFile lsp method
    Define and implement the experimental/gotoRelevantFile lsp method. This
    request takes in a textDcoument param, and retusn an array of locations
    that are related to the input file path.
    
    Currently, the related file paths are
    - test files, if the input file is a source file
    - source files, if the input file is a test file
    
    Basically it goes between source file <> test file.
    
    This is implemented as a experimental method until something more
    official comes along.

--- lib/ruby_lsp/server.rb
+++ lib/ruby_lsp/server.rb
@@ -112,6 +112,8 @@ module RubyLsp
         diagnose_state(message)
       when "rubyLsp/discoverTests"
         discover_tests(message)
+      when "experimental/gotoRelevantFile"
+        experimental_goto_relevant_file(message)
       when "$/cancelRequest"
         @global_state.synchronize { @cancelled_requests << message[:params][:id] }
       when nil
@@ -290,6 +292,7 @@ module RubyLsp
           experimental: {
             addon_detection: true,
             compose_bundle: true,
+            goto_relevant_file: true,
           },
         ),
         serverInfo: {
@@ -1119,6 +1122,25 @@ module RubyLsp
           document,
           params[:range],
         ).perform,
+      }
+      send_message(Result.new(id: message[:id], response: response))
+    end
+
+    sig { params(message: T::Hash[Symbol, T.untyped]).void }
+    def experimental_goto_relevant_file(message)
+      path = message.dig(:params, :textDocument, :uri).to_standardized_path
+      unless path.nil? || path.start_with?(@global_state.workspace_path)
+        send_empty_response(message[:id])
+        return
+      end
+
+      unless path
+        send_empty_response(message[:id])
+        return
+      end
+
+      response = {
+        locations: Requests::GotoRelevantFile.new(path).perform,
       }
       send_message(Result.new(id: message[:id], response: response))
     end
