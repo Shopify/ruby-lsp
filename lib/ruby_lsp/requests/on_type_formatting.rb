1d799552dce997880cf9ffa42633703685ae2ec7
vinistock <18742907+vinistock@users.noreply.github.com>
2025-01-29 22:22:35 +0000

    Allow snippets for Cursor editor (#3109)
    ### Motivation
    
    Fixes the behaviour described in https://github.com/Shopify/ruby-lsp/discussions/3094#discussioncomment-11997299.
    
    While the LSP spec doesn't standardize snippet support, we use a custom middleware to allow moving the user's cursor around when applying on type formatting. We were checking to apply this only in VS Code, but not Cursor.
    
    ### Implementation
    
    Added Cursor to the mix.
    
    ### Automated Tests
    
    Added a test.

--- lib/ruby_lsp/requests/on_type_formatting.rb
+++ lib/ruby_lsp/requests/on_type_formatting.rb
@@ -170,7 +170,7 @@ module RubyLsp
 
       sig { params(line: Integer, character: Integer).void }
       def move_cursor_to(line, character)
-        return unless @client_name.start_with?("Visual Studio Code")
+        return unless /Visual Studio Code|Cursor/.match?(@client_name)
 
         position = Interface::Position.new(
           line: line,
