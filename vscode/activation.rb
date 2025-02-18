75b5dad1d674311fe4186c997f083c9d764f5ad1
Ryan O'Neill <ryan@aboon.com>
2025-02-18 14:42:34 -0500

    Coerce non UTF-8 separators to UTF-8

--- vscode/activation.rb
+++ vscode/activation.rb
@@ -1,3 +1,3 @@
 env = ENV.filter_map { |k, v| "#{k}RUBY_LSP_VS#{v}" if v.dup.force_encoding(Encoding::UTF_8).valid_encoding? }
 env.unshift(RUBY_VERSION, Gem.path.join(","), !!defined?(RubyVM::YJIT))
-STDERR.print("RUBY_LSP_ACTIVATION_SEPARATOR#{env.join("RUBY_LSP_FS")}RUBY_LSP_ACTIVATION_SEPARATOR")
+STDERR.print("RUBY_LSP_ACTIVATION_SEPARATOR#{env.join("RUBY_LSP_FS")}RUBY_LSP_ACTIVATION_SEPARATOR".encode('UTF-8', invalid: :replace, undef: :replace))
