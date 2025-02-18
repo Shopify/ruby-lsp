3a06df327952c9aef83cc41ee2919c129739337c
Andy Waite <13400+andyw8@users.noreply.github.com>
2025-02-18 11:46:18 -0500

    Fix naming

--- lib/minitest/reporters/ruby_lsp_reporter.rb
+++ lib/minitest/reporters/ruby_lsp_reporter.rb
@@ -1,6 +1,7 @@
 # typed: strict
 # frozen_string_literal: true
 
+require "sorbet-runtime"
 require "ruby_lsp/test_reporter"
 
 # NOTE: minitest-reporters mentioned an API change between minitest 5.10 and 5.11, so we should verify:
