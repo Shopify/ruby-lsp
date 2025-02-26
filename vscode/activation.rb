920765673a5cfaf884f2c9c311172825b07d1e12
Sam Bostock <sam.bostock@shopify.com>
2025-02-26 18:58:14 +0000

    Use `.map { ... }.compact` in activation script
    `filter_map` was introduced in Ruby 2.7, but this script may be run by
    an ancient Ruby version.

--- vscode/activation.rb
+++ vscode/activation.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-env = ENV.filter_map do |k, v|
+env = ENV.map do |k, v|
   utf_8_value = v.dup.force_encoding(Encoding::UTF_8)
   "#{k}RUBY_LSP_VS#{utf_8_value}" if utf_8_value.valid_encoding?
-end
+end.compact
 env.unshift(RUBY_VERSION, Gem.path.join(","), !!defined?(RubyVM::YJIT))
 $stderr.print("RUBY_LSP_ACTIVATION_SEPARATOR#{env.join("RUBY_LSP_FS")}RUBY_LSP_ACTIVATION_SEPARATOR")
