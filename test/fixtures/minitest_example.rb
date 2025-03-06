a205912d9ca9c3c31577d23a71dfe460d66c3ade
Andy Waite <andy.waite@shopify.com>
2025-03-06 12:35:16 -0500

    Add JSON test reporter for minitest (#3187)
    * Add JSON test runner for Minitest
    
    * Nicer parse_output
    
    * Handle output differences in newer Ruby
    
    * Remove unnecessary `super` call
    
    * Adjust method visibility
    
    * Rename test class
    
    * Use shared helper
    
    * Don't rely on repo test_helper for fixture
    
    * Re-enable reporters
    
    * Remove comment

---
+++ test/fixtures/minitest_example.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+require "minitest/autorun"
+require_relative "../../lib/ruby_indexer/lib/ruby_indexer/uri"
+
+# We are only testing the output of the runner, there's no need for to be random.
+Minitest::Test.i_suck_and_my_tests_are_order_dependent!
+
+class SampleTest < Minitest::Test
+  def test_that_passes
+    assert_equal(1, 1)
+    assert_equal(2, 2)
+  end
+
+  def test_that_fails
+    assert_equal(1, 2)
+  end
+
+  def test_that_is_pending
+    skip("pending test")
+  end
+
+  def test_that_raises
+    raise "oops"
+  end
+end
--- test/minitest_test_runner_test.rb
+++ test/minitest_test_runner_test.rb
@@ -2,25 +2,26 @@
 # frozen_string_literal: true
 
 require "test_helper"
-require "stringio"
 
 module RubyLsp
-  class TestUnitTestRunnerTest < Minitest::Test
-    def test_test_runner_output
-      _stdin, stdout, _stderr, _wait_thr = Open3.popen3(
+  class MinitestTestRunnerTest < Minitest::Test
+    def test_minitest_output
+      plugin_path = "lib/ruby_lsp/ruby_lsp_reporter_plugin.rb"
+      env = { "RUBYOPT" => "-r./#{plugin_path}" }
+      _stdin, stdout, _stderr, _wait_thr = T.unsafe(Open3).popen3(
+        env,
         "bundle",
         "exec",
         "ruby",
-        "test/fixtures/test_unit_example.rb",
-        "--runner",
-        "ruby_lsp",
+        "-Itest",
+        "test/fixtures/minitest_example.rb",
       )
       stdout.binmode
       stdout.sync = true
 
       actual = parse_json_api_stream(stdout)
 
-      uri = URI::Generic.from_path(path: "#{Dir.pwd}/test/fixtures/test_unit_example.rb").to_s
+      uri = URI::Generic.from_path(path: "#{Dir.pwd}/test/fixtures/minitest_example.rb").to_s
       expected = [
         {
           "method" => "start",
@@ -33,7 +34,7 @@ module RubyLsp
           "method" => "fail",
           "params" => {
             "id" => "SampleTest#test_that_fails",
-            "message" => "<1> expected but was\n<2>.",
+            "message" => "Expected: 1\n  Actual: 2",
             "uri" => uri,
           },
         },
@@ -77,12 +78,25 @@ module RubyLsp
           "method" => "error",
           "params" => {
             "id" => "SampleTest#test_that_raises",
-            "message" => "RuntimeError: oops",
+            "message" => "RuntimeError: oops\n    test/fixtures/minitest_example.rb:24:in #{error_location}",
             "uri" => uri,
           },
         },
       ]
+      assert_equal(8, actual.size)
       assert_equal(expected, actual)
+    end
+
+    private
+
+    def error_location
+      ruby_version = Gem::Version.new(RUBY_VERSION)
+
+      if ruby_version >= Gem::Version.new("3.4")
+        "'SampleTest#test_that_raises'"
+      else
+        "`test_that_raises'"
+      end
     end
   end
 end
