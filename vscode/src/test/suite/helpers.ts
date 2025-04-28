60b137912bb33aafb465a6e3c56d5f5362ac7296
Vinicius Stock <vinistock@users.noreply.github.com>
2025-04-28 15:01:42 -0400

    Deduplicate code in testController.test.ts (#3423)

--- vscode/src/test/suite/helpers.ts
+++ vscode/src/test/suite/helpers.ts
@@ -3,6 +3,8 @@ import path from "path";
 import os from "os";
 import fs from "fs";
 
+import * as vscode from "vscode";
+
 import { MAJOR, MINOR, RUBY_VERSION } from "../rubyVersion";
 
 export function createRubySymlinks() {
@@ -41,3 +43,22 @@ export function createRubySymlinks() {
     }
   }
 }
+
+export const LSP_WORKSPACE_PATH = path.dirname(
+  path.dirname(path.dirname(path.dirname(__dirname))),
+);
+export const LSP_WORKSPACE_URI = vscode.Uri.file(LSP_WORKSPACE_PATH);
+export const LSP_WORKSPACE_FOLDER: vscode.WorkspaceFolder = {
+  uri: LSP_WORKSPACE_URI,
+  name: path.basename(LSP_WORKSPACE_PATH),
+  index: 0,
+};
+export const CONTEXT = {
+  extensionMode: vscode.ExtensionMode.Test,
+  subscriptions: [],
+  workspaceState: {
+    get: (_name: string) => undefined,
+    update: (_name: string, _value: any) => Promise.resolve(),
+  },
+  extensionUri: vscode.Uri.joinPath(LSP_WORKSPACE_URI, "vscode"),
+} as unknown as vscode.ExtensionContext;
