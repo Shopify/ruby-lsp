7e42cbc1bec42bbad0a7dc1a81b1ae138a031aad
Rafael Mendonça França <rafael.franca@shopify.com>
2026-01-12 18:38:46 -0500

    Implement Rv using Mise as base class

--- vscode/src/ruby/rv.ts
+++ vscode/src/ruby/rv.ts
@@ -1,14 +1,13 @@
 import * as vscode from "vscode";
 
-import { VersionManager, ActivationResult } from "./versionManager";
 import { pathToUri } from "../common";
-import { WorkspaceChannel } from "../workspaceChannel";
+import { Mise } from "./mise";
 
 // Manage your Ruby environment with rv
 //
 // Learn more: https://github.com/spinel-coop/rv
-export class Rv extends VersionManager {
-  private static getPossiblePaths(): vscode.Uri[] {
+export class Rv extends Mise {
+  protected static getPossiblePaths(): vscode.Uri[] {
     return [
       pathToUri("/", "home", "linuxbrew", ".linuxbrew", "bin", "rv"),
       pathToUri("/", "usr", "local", "bin", "rv"),
@@ -17,27 +16,15 @@ export class Rv extends VersionManager {
     ];
   }
 
-  static async detect(
-    _workspaceFolder: vscode.WorkspaceFolder,
-    _outputChannel: WorkspaceChannel,
-  ): Promise<vscode.Uri | undefined> {
-    return VersionManager.findFirst(Rv.getPossiblePaths());
+  protected getVersionManagerName(): string {
+    return "Rv";
   }
 
-  async activate(): Promise<ActivationResult> {
-    const rvExec = await this.findVersionManagerUri(
-      "Rv",
-      "rubyVersionManager.rvExecutablePath",
-      Rv.getPossiblePaths(),
-      () => Rv.detect(this.workspaceFolder, this.outputChannel),
-    );
-    const parsedResult = await this.runEnvActivationScript(`${rvExec.fsPath} ruby run --`);
+  protected getConfigKey(): string {
+    return "rubyVersionManager.rvExecutablePath";
+  }
 
-    return {
-      env: { ...process.env, ...parsedResult.env },
-      yjit: parsedResult.yjit,
-      version: parsedResult.version,
-      gemPath: parsedResult.gemPath,
-    };
+  protected getExecutionCommand(executablePath: string): string {
+    return `${executablePath} ruby run --`;
   }
 }
