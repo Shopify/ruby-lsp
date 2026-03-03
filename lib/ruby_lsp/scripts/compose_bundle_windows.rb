e42bf9d89323fd8c89835e7934e73ebdd7800ff1
Vinicius Stock <vinicius.stock@shopify.com>
2026-03-03 10:20:26 -0500

    Ensure the original CLI arguments are used when updating

--- lib/ruby_lsp/scripts/compose_bundle_windows.rb
+++ lib/ruby_lsp/scripts/compose_bundle_windows.rb
@@ -5,4 +5,8 @@ require_relative "compose_bundle"
 
 # When this is invoked on Windows, we pass the raw initialize as an argument to this script. On other platforms, we
 # invoke the compose method from inside a forked process
-compose(ARGV.first, beta: ARGV.include?("--beta"))
+options = {}
+options[:beta] = true if ARGV.include?("--beta")
+branch_index = ARGV.index("--branch")
+options[:branch] = ARGV[branch_index + 1] if branch_index
+compose(ARGV.first, **options)
